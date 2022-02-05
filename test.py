1b5683e92053b5cf1c92f79a2c7f6b13f71140eb
neko113 <75781414+neko113@users.noreply.github.com>
2022-02-06 01:29:57 +0900

    프로필

--- test.py
+++ test.py
@@ -1,54 +1,23 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
 
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-def bfs(x, y):
-    queue = deque([(x, y)])
-    visited[x][y] = 0
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            # 동 남 서 북 순서
-            nx, ny = x + dx[i], y + dy[i]
-            while True:
-                # 범위를 벗어난다
-                if not (0 <= nx < n and 0 <= ny < m):
-                    break
-                # 벽을 만난다
-                if board[nx][ny] == "*":
-                    break
-                # 지난 적 있는 곳인데, 지금 경로로는 너무 많은 거울이 필요해서 break
-                if visited[nx][ny] < visited[x][y] + 1:
-                    break
-                # board업데이트, queue 추가
-                queue.append((nx, ny))
-                visited[nx][ny] = visited[x][y] + 1
-                nx = nx + dx[i]
-                ny = ny + dy[i]
+dp = [[INF]*n for _ in range(n)]
+dp[0][0] = 0
+for i in range(n):
+    for j in range(n):
+        for d in ((1, 0), (0, 1)):
+            nx = i+d[0]
+            ny = j+d[1]
+            if n <= nx or nx < 0 or n <= ny or ny < 0:
+                continue
+            v = 0
+            if arr[i][j] <= arr[nx][ny]:
+                v = arr[nx][ny]-arr[i][j]+1
 
+            dp[nx][ny] = min(dp[nx][ny], v+dp[i][j])
 
-if __name__ == "__main__":
-    # 입력값
-    m, n = map(int, input().split())
-    board = [input() for _ in range(n)]
-
-    # 동 남 서 북
-    dx = (0, 1, 0, -1)
-    dy = (1, 0, -1, 0)
-
-    # C위치
-    C = []
-    for i in range(n):
-        for j in range(m):
-            if board[i][j] == "C":
-                C.append((i, j))
-    # sx,sy : 시작지점
-    # ex,ey : 도착지점
-    (sx, sy), (ex, ey) = C
-
-    visited = [[float("inf")] * m for _ in range(n)]
-    bfs(sx, sy)
-
-    print(visited[ex][ey] - 1)
-
-
-# 코드 참고 : https://www.acmicpc.net/source/27251914
+print(dp[n-1][n-1])
