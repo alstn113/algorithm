0601cddbc30d47b8f6464d0080845269c0a3d8c5
neko113 <alstn113@gmail.com>
2022-03-12 20:13:11 +0900

    순열

---
+++ example/순열/순열.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+
+# arr 배열에서 n개를 순서있게 뽑음
+def permutation(n, depth, P):
+    result = []
+    if depth == n:
+        return [P]
+    else:
+        for i in range(len(arr)):
+            if chosen[i] == True:
+                continue
+            chosen[i] = True
+            result += permutation(n, depth+1, P+[i])
+            chosen[i] = False
+    return result
+
+
+arr = [1, 2, 3]
+chosen = [False for _ in range(len(arr))]
+
+# 3개 뽑은 인덱스를 배열로 반환
+print(permutation(3, 0, []))
--- test.py
+++ test.py
@@ -1,16 +1,11 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+INF = 1e9
 
-N, M = map(int, input().split())
-H_list = list(map(int, input().split()))
-T = [[0]*N for _ in range(M)]
-for i in range(M):
-    a, b, k = map(int, input().split())
-    for j in range(a-1, (b-1)+1):
-        T[i][j] += k
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+min_bridge = INF
 
 for i in range(N):
-    for j in range(M):
-        H_list[i] += T[j][i]
-print(" ".join(map(str, H_list)))
+    for j in range(N):
+        if arr[i][j]
