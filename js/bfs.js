57bcc381c5cc9b106ca2c6a6f094340ebfb3ead4
neko113 <alstn113@gmail.com>
2022-07-25 18:01:51 +0900

    feat : js dfs/bjs algo

---
+++ js/bfs.js
@@ -0,0 +1,32 @@
+const graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7],
+];
+// 각 인덱스에 연결된 노드
+const visited = Array(9).fill(false);
+let result = '';
+
+bfs(graph, 1, visited);
+console.log(result);
+
+function bfs(graph, start, visited) {
+  const queue = [start];
+  visited[start] = true;
+  while (queue.length !== 0) {
+    v = queue.shift();
+    result += `${v} `;
+    for (i of graph[v]) {
+      if (!visited[i]) {
+        queue.push(i);
+        visited[i] = true;
+      }
+    }
+  }
+}
---
+++ js/dfs.js
@@ -0,0 +1,27 @@
+const graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7],
+];
+// 각 인덱스에 연결된 노드
+const visited = Array(9).fill(false);
+let result = '';
+
+dfs(graph, 1, visited);
+console.log(result);
+
+function dfs(graph, v, visited) {
+  visited[v] = true;
+  result += `${v} `;
+
+  for (i of graph[v]) {
+    if (!visited[i]) dfs(graph, i, visited);
+    // 그 노드가 방문하지 않은 노드인면 dfs 재귀
+  }
+}
