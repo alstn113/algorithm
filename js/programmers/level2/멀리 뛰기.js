2c0740a3ed0b13c829471cccad94e6a2751b2a95
neko113 <alstn113@gmail.com>
2022-08-13 16:05:59 +0900

    feat : 08/13 [멀리 뛰기]

--- js/programmers/level2/멀리 뛰기.js
+++ js/programmers/level2/멀리 뛰기.js
@@ -1,7 +1,11 @@
 function solution(n) {
-  // n === 2 경우는 2, 1+1이 가능
-  if (n === 1 || n === 2) return n;
-  return (solution(n - 2) + solution(n - 1)) % 1234567;
+  const dp = new Array(2000).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+  }
+  return dp[n] % 1234567;
 }
 
 console.log(solution(4));
