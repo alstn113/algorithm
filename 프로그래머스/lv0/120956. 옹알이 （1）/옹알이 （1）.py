5fe31ec9775d8a3f5e1c7b28f4c481f6f40cb234
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-05-01 02:31:18 +0900

    [level 0] Title: 옹알이 (1), Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,14 @@
+from itertools import permutations
+
+def solution(babbling):
+    data = []
+    able_to_say = ["aya", "ye", "woo", "ma"]
+    for i in range(1, 4+1):
+        for p in list(permutations(able_to_say,i)):
+            data.append("".join(p))
+    
+    cnt = 0
+    for b in babbling:
+        if b in data:
+            cnt += 1
+    return cnt
