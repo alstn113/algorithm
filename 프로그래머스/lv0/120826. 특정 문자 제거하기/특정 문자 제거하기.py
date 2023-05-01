12e526f369effbbf3457f06d7280d54fe8ffa85b
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-05-01 12:44:55 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,6 +1,11 @@
 def solution(my_string, letter):
-    answer = ""
-    for i in my_string:
-        if i != letter:
-            answer += i
-    return answer
+    return my_string.replace(letter, "")
+
+# 기존 풀이 
+# 
+# def solution(my_string, letter):
+#     answer = ""
+#     for i in my_string:
+#         if i != letter:
+#             answer += i
+#     return answer
