4a01b152a345443b6121b8512d795f02ff5aae5d
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-05-01 14:17:22 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,9 @@
+def solution(dot):
+    if dot[0] > 0 and dot[1] > 0:
+        return 1
+    elif dot[0] < 0 and dot[1] > 0:
+        return 2
+    elif dot[0] < 0 and dot[1] < 0:
+        return 3
+    else: 
+        return 4
