e47dc944218de4f61d9fbbf0a18f78efd6a3f254
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-05-02 12:09:32 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,15 @@
+def solution(my_string):
+    answer = 0
+    tmp = ""
+    for i in my_string:
+        if i.isnumeric():
+            tmp += i
+        elif tmp != "":
+            answer += int(tmp)
+            tmp = ""
+    if tmp != "":
+        answer += int(tmp)
+    
+    return answer
+    
+            
