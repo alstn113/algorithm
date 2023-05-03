d227b4f743f30f0453361813bb41df483812eec9
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-05-03 21:08:38 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,20 +1,12 @@
 def solution(common):
-    flag = True # 등차수열
-    tmp = 0
-    for i in range(0,len(common)-3+1, 3):
-        a, b, c = common[i], common[i+1], common[i+2]
-        if a + c ==  2*b:
-            flag = True
-            tmp = b-a
-        elif a*c == b**2:
-            flag = False
-            tmp = b//a
+    a, b, c = common[0], common[1], common[2]
+    if a + c ==  2*b:
+        return common[-1] + b-a
+    elif a*c == b**2:
+        return common[-1]*(b//a)
     
-    print(flag, tmp)
     
-    if flag:
-        return common[-1] + tmp
-    else:
-        return common[-1]*tmp
+    
+
             
             
