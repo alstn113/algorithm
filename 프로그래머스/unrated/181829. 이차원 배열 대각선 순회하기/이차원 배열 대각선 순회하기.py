deaa9c92263bb95dec4aaea9039e827a0d2437e2
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-05-04 09:11:06 +0900

    [unrated] Title: 이차원 배열 대각선 순회하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
@@ -0,0 +1,9 @@
+def solution(board, k):
+    answer = 0
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if i + j <= k:
+                answer += board[i][j]
+
+    
+    return answer
