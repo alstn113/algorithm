a8da6e25cfd70ac3239f44a63eba7bd6f4762239
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-05-05 21:38:28 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,18 +1,27 @@
-def dfs(number, data):
-    global cnt
+from itertools import combinations
 
-    if len(data) == 3 and sum(data) == 0:
-        cnt += 1
-        return
-
-    for i in range(len(number)):
-        dfs(number[i+1:], data+[number[i]])
-
-
-def solution(number):
-    global cnt
+def solution(number) :
     cnt = 0
-
-    dfs(number, [])
-
+    for c in combinations(number,3) :
+        if sum(c) == 0 :
+            cnt += 1
     return cnt
+
+# def dfs(number, data):
+#     global cnt
+
+#     if len(data) == 3 and sum(data) == 0:
+#         cnt += 1
+#         return
+
+#     for i in range(len(number)):
+#         dfs(number[i+1:], data+[number[i]])
+
+
+# def solution(number):
+#     global cnt
+#     cnt = 0
+
+#     dfs(number, [])
+
+#     return cnt
