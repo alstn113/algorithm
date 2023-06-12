d09cc374f429e9734fbe37bd3f36777b8d965025
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-06-13 00:45:12 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,27 @@
+def compress_str(s, l):
+    cnt = 1
+    data = [s[:l]]
+    for i in range(l, len(s), l):
+        v = s[i:i+l]
+        if data[-1] == v:
+            cnt += 1
+        else:
+            if cnt > 1:
+                data.append(str(cnt))
+                cnt = 1
+            data.append(v)
+    if cnt > 1:
+        data.append(str(cnt))
+    
+    compressed_str = "".join(data)
+    return len(compressed_str)
+        
+
+def solution(s):
+    str_len = len(s)
+    answer = str_len
+    for i in range(1, str_len):
+        compressed_str = compress_str(s, i)
+        answer = min(answer, compressed_str)
+        
+    return answer
