e72e75d6ce166647081727c5b1366d5afb964f77
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-06-15 00:07:24 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,23 +1,26 @@
-def dfs(x, y, place, distance, visited):
-    if distance == 2:
-        return False
-    
+def dfs(x, y, place):
     for (dx, dy) in ((0,1), (0,-1), (-1,0), (1,0)):
         nx = x + dx
         ny = y + dy
         
-        if not(0<=nx<5 and 0<=ny<5):
-            continue
-        
-        if place[nx][ny] == "X":
+        if not(0<=nx<5 and 0<=ny<5) or place[nx][ny] == "X":
             continue
             
-        if place[nx][ny] == "P" and not visited[nx][ny]:
+        if place[nx][ny] == "P":
             return True
-        
-        if dfs(nx, ny, place, distance+1, visited):
-            return True
-    
+            
+        for (tx, ty) in ((0,1), (0,-1), (-1,0), (1,0)):
+            n2x = nx + tx
+            n2y = ny + ty
+            
+            if dx+tx == 0 and dy+ty == 0:
+                continue
+            if not(0<=n2x<5 and 0<=n2y<5) or place[nx][ny] == "X":
+                continue
+            
+            if place[n2x][n2y] == "P":
+                return True
+            
     return False
         
         
@@ -27,13 +30,11 @@ def dfs(x, y, place, distance, visited):
 def solution(places):
     answer = []
     for place in places:
-        visited = [[False]*5 for _ in range(5)]
         flag = False
         for i in range(5):
             for j in range(5):
                 if place[i][j] == "P":
-                    visited[i][j] = True
-                    if dfs(i,j,place,0, visited):       
+                    if dfs(i,j,place):       
                         flag = True 
                         break
             if flag:
