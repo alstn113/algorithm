0959b34372882c479bc43ec7b5c7e9c6698145f2
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-06-14 16:03:13 +0900

    [level 2] Title: 후보키, Time: 0.19 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -1,34 +1,35 @@
 from itertools import combinations
 
 def solution(relation):
-    l = len(relation[0])
-    data = []
-    for i in range(1,l+1):
-        a = [v for v in range(l)]
-        data += list(combinations(a,i)) 
-    
+    column = [i for i in range(len(relation[0]))]
     result = []
-    for d in data:
-        # 여기는 최소성 체크
+    
+    # 가능한 조합
+    comb = []
+    for i in range(1, len(column)+1):
+        comb += list(combinations(column, i))
+
+    for c in comb:
+        # 최소성
         flag = False
         for r in result:
-            if set(r) | set(d) == set(d):
+            if set(r) & set(c) == set(r):
                 flag = True
                 break
         if flag:
             continue
         
-        # 여기는 유일성 체크
-        b = []
+        # 유일성
+        tmp = []
+        flag = False
         for i in range(len(relation)):
-            tmp = []
-            for j in d:
-                tmp.append(relation[i][j])
-            tmp = " ".join(tmp)
-            b.append(tmp)
-        
-        if len(b) == len(list(set(b))):
-            result.append(d)
+            comb_column = " ".join([relation[i][v] for v in c])
+            if comb_column not in tmp:
+                tmp.append(comb_column)
+            else:
+                flag = True
+                break
+        if not flag:
+            result.append(c)
     
     return len(result)
-            
