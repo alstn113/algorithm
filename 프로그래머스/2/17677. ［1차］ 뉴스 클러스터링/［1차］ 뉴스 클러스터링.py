890c0ff3fc9708c39f2e145ada24e572199e3cc0
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-10-30 14:25:07 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,16 +1,37 @@
-def solution(str1, str2):
-    s1 = [str1[i:i+2].lower()
-          for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
-    s2 = [str2[i:i+2].lower()
-          for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
+def make_two_english_letters_list(s: str):
+    result = []
+    for i in range(len(s)-1):
+        v = s[i:i+2]
+        if v.isalpha():
+            result.append(v.lower())
+    return result
 
+
+def make_jacard_intersection(s1, s2, intersection):
+    result = 0
+    for v in intersection:
+        result += min(s1.count(v), s2.count(v))
+    return result
+
+
+def make_jacard_union(s1, s2, union):
+    result = 0
+    for v in union:
+        result += max(s1.count(v), s2.count(v))
+    return result
+
+
+def solution(str1, str2):
+    BIG_NUM = 65536
+    s1 = make_two_english_letters_list(str1)
+    s2 = make_two_english_letters_list(str2)
     intersection = set(s1) & set(s2)
     union = set(s1) | set(s2)
 
     if len(union) == 0:
-        return 65536
+        return BIG_NUM
 
-    m_intersection = sum([min(s1.count(i), s2.count(i)) for i in intersection])
-    m_union = sum([max(s1.count(u), s2.count(u)) for u in union])
+    jacard_intersection = make_jacard_intersection(s1, s2, intersection)
+    jacard_union = make_jacard_union(s1, s2, union)
 
-    return int(65536 * (m_intersection / m_union))
+    return int(BIG_NUM * (jacard_intersection / jacard_union))
