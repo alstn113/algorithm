bf326ae80356214b0eee0ed983f4d57d470cd083
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-10-31 00:28:51 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,42 +1,42 @@
 from collections import deque
 
 
-def find_robot_and_target(board):
+def find_robot(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == "R":
                 return i, j
 
+def slide(x, y, board, dx, dy):
+    while True:
+        x += dx
+        y += dy
+
+        is_over_block = x == -1 or y == - \
+            1 or x == len(board) or y == len(board[0])
+        if is_over_block or board[x][y] == "D":
+            return x-dx, y-dy
 
 def solution(board):
-    robot_x, robot_y = find_robot_and_target(board)
+    robot_x, robot_y = find_robot(board)
     r, c = len(board), len(board[0])
-    queue = deque([(robot_x, robot_y, 0)])
-    visited = [[False]*c for _ in range(r)]
-    visited[robot_x][robot_y] = True
+    queue = deque([(robot_x, robot_y)])
+    visited = [[-1]*c for _ in range(r)]
+    visited[robot_x][robot_y] = 0
 
     while queue:
-        x, y, cnt = queue.popleft()
+        x, y = queue.popleft()
 
         if board[x][y] == "G":
-            return cnt
+            return visited[x][y]
 
         for (dx, dy) in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-            nx = x
-            ny = y
+            nx, ny = slide(x, y, board, dx, dy)
 
-            while True:
-                nx += dx
-                ny += dy
+            if visited[nx][ny] != -1:
+                continue
 
-                is_over_block = nx == -1 or ny == -1 or nx == r or ny == c
-                if is_over_block or board[nx][ny] == "D":
-                    nx -= dx
-                    ny -= dy
+            queue.append((nx, ny))
+            visited[nx][ny] = visited[x][y] + 1
 
-                    if visited[nx][ny]:
-                        break
-
-                    queue.append((nx, ny, cnt+1))
-                    visited[nx][ny] = True
     return -1
