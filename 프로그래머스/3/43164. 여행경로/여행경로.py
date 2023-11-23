64a83775debc99122ff83e037228da874907ab7b
김민수 / Minsoo Kim <alstn113@gmail.com>
2023-11-23 17:02:13 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,26 +1,71 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/43164
-# 여행경로
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+from collections import defaultdict 
+
+def dfs(graph, N, key, footprint):
+    print(footprint)
+
+    if len(footprint) == N + 1:
+        return footprint
+
+    for idx, country in enumerate(graph[key]):
+        graph[key].pop(idx)
+
+        tmp = footprint[:]
+        tmp.append(country)
+
+        ret = dfs(graph, N, country, tmp)
+
+        graph[key].insert(idx, country)
+
+        if ret:
+            return ret
+
 
 def solution(tickets):
     answer = []
-    visited = [False]*len(tickets)
 
-    def dfs(current, path):
-        if len(path) == len(tickets)+1:
-            answer.append(path)
-            return
+    graph = defaultdict(list)
 
-        for idx, ticket in enumerate(tickets):
-            if visited[idx]:
-                continue
+    N = len(tickets)
+    for ticket in tickets:
+        graph[ticket[0]].append(ticket[1])
+        graph[ticket[0]].sort()
 
-            a, b = ticket
-            if current == a:
-                visited[idx] = True
-                dfs(b, path + [b])
-                visited[idx] = False
+    answer = dfs(graph, N, "ICN", ["ICN"])
 
-    dfs("ICN", ["ICN"])
-    answer.sort()
-
-    return answer[0]
+    return answer
