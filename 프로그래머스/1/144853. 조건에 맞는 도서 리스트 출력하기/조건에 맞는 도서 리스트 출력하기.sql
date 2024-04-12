36c8bd68b638c32dccac5c3b6298622ae261624d
김민수 / Minsu Kim <alstn113@gmail.com>
2024-04-13 08:37:52 +0900

    [level 1] Title: 조건에 맞는 도서 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
+++ 프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
@@ -1,4 +1,4 @@
-SELECT BOOK_ID, DATE_FORMAT(PUBLISHED_DATE, "%Y-%m-%d") as PUBLISHED_DATE
-FROM BOOK
-WHERE CATEGORY = '인문' AND YEAR(PUBLISHED_DATE) = 2021
-ORDER BY PUBLISHED_DATE ASC
+select book_id, date_format(published_date, "%Y-%m-%d") as published_date
+from book
+where category = "인문" and year(published_date) = 2021
+order by published_date asc
