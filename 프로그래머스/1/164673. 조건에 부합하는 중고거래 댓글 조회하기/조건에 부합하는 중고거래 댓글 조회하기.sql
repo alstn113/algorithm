593febcccc4d51bffd23bcdb3cc22ffe873eb356
김민수 / Minsu Kim <alstn113@gmail.com>
2024-04-13 08:52:46 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,12 +1,12 @@
-SELECT 
-    B.TITLE, 
-    B.BOARD_ID, 
-    R.REPLY_ID, 
-    R.WRITER_ID, 
-    R.CONTENTS, 
-    DATE_FORMAT(R.CREATED_DATE,"%Y-%m-%d") AS CREATED_DATE
-FROM USED_GOODS_BOARD AS B  
-JOIN USED_GOODS_REPLY AS R 
-ON B.BOARD_ID = R.BOARD_ID 
-WHERE B.CREATED_DATE LIKE "%2022-10%"
-ORDER BY R.CREATED_DATE ASC,  B.TITLE ASC
+select
+    b.title,
+    b.board_id,
+    r.reply_id,
+    r.writer_id,
+    r.contents,
+    date_format(r.created_date, "%Y-%m-%d") as created_date
+from used_goods_board as b
+join used_goods_reply as r
+on b.board_id = r.board_id
+where b.created_date like "%2022-10%"
+order by created_date asc, title asc 
