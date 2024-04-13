9a5764111efb57082df67da8e86a499b11d0cd7f
김민수 / Minsu Kim <alstn113@gmail.com>
2024-04-13 12:25:41 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,9 +1,8 @@
-SELECT
-    ANIMAL_ID,
-    NAME,
-    CASE 
-        WHEN SEX_UPON_INTAKE LIKE "%Neutered%" 
-            OR SEX_UPON_INTAKE LIKE "%Spayed%" THEN "O"
-        ELSE "X"
-    END AS "중성화"
-FROM ANIMAL_INS
+select 
+    animal_id, 
+    name,
+    case 
+        when sex_upon_intake like "%Neutered%" or sex_upon_intake like "%Spayed%" then "O"
+        else "X"
+    end as "중성화"
+from animal_ins
