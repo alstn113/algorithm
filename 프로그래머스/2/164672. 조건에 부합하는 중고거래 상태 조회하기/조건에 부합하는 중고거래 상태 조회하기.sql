001df718a56cc937ad21e4fa53d22938cdbc8f99
김민수 / Minsu Kim <alstn113@gmail.com>
2024-04-13 12:01:12 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,13 +1,13 @@
-SELECT 
-    BOARD_ID,
-    WRITER_ID,
-    TITLE,
-    PRICE,
-    CASE 
-        WHEN STATUS = "SALE" THEN "판매중"
-        WHEN STATUS = "RESERVED" THEN "예약중"
-        WHEN STATUS = "DONE" THEN "거래완료"
-    END AS STATUS
-FROM USED_GOODS_BOARD
-WHERE CREATED_DATE = "2022-10-05"
-ORDER BY BOARD_ID DESC
+select
+    board_id,
+    writer_id,
+    title,
+    price,
+    case
+        when status = "SALE" then "판매중"
+        when status = "RESERVED" then "예약중"
+        when status = "DONE" then "거래완료"
+    end as status
+from used_goods_board
+where created_date = "2022-10-05"
+order by board_id desc
