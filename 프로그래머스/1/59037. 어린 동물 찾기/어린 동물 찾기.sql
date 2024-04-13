fbb13ec43a75c68c27c0f123064dc1603bcd8096
김민수 / Minsu Kim <alstn113@gmail.com>
2024-04-13 09:50:53 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,4 +1,4 @@
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-WHERE INTAKE_CONDITION != 'Aged'
-ORDER BY ANIMAL_ID;
+select animal_id, name
+from animal_ins
+where intake_condition != "Aged"
+order by animal_id
