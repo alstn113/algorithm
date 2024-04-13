5ef8132ac1b2e74473393262fd27ab90c00fbcc0
김민수 / Minsu Kim <alstn113@gmail.com>
2024-04-13 12:14:18 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,4 @@
-SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
-FROM ANIMAL_INS
-WHERE NAME IN ("Lucy", "Ella", "Pickle", "Rogan", "Sabrina", "Mitty")
-ORDER BY ANIMAL_ID
+select animal_id, name, sex_upon_intake
+from animal_ins
+where name in ("Lucy", "Ella", "Pickle", "Rogan", "Sabrina", "Mitty")
+order by animal_id
