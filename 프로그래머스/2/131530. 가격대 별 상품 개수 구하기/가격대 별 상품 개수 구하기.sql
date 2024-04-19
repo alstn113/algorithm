e368e9ca41eea904580f67e1398e15fab5b7e76e
김민수 / Minsu Kim <alstn113@gmail.com>
2024-04-19 14:22:08 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,4 +1,6 @@
-SELECT FLOOR(PRICE/10000)*10000 AS PRICE_GROUP, COUNT(*) AS PRODUCTS
-FROM PRODUCT
-GROUP BY PRICE_GROUP
-ORDER BY PRICE_GROUP ASC
+select 
+    floor(price/10000)*10000 as price_group,
+    count(*) as products
+from product
+group by price_group
+order by price_group asc
