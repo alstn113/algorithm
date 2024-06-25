223e9d0e0b1bf3003d26da4d8bf20b487d4ee66b
김민수 / Minsu Kim <alstn113@gmail.com>
2024-06-25 10:58:44 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] result = new int[n];
+        for(int i=0; i<n; i++) {
+            result[i] = num_list[i];
+        }
+        return result;
+    }
+}
