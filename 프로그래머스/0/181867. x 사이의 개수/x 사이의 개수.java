a4ec2ee76a30f694afc336992b668206e8c9078c
김민수 / Minsu Kim <alstn113@gmail.com>
2024-06-25 10:55:33 +0900

    [level 0] Title: x 사이의 개수, Time: 8.10 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String myString) {
+        String[] result = myString.split("x", -1);
+        return Arrays.stream(result).map(String::length).mapToInt(Integer::intValue).toArray();
+    }
+}
