830321161af3fb47b727499f20cc389ced2df2e1
김민수 / Minsu Kim <alstn113@gmail.com>
2024-06-26 10:25:04 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String myString) {
+        myString = myString.toLowerCase();
+        myString = myString.replace("a", "A");
+        return myString;
+    }
+}
