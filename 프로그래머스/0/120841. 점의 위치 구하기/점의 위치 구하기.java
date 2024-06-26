0ba87df3ea3ae560d608ac13c782803b94c35f87
김민수 / Minsu Kim <alstn113@gmail.com>
2024-06-26 10:20:18 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] dot) {
+        if (dot[0] > 0 && dot[1] > 0) return 1;
+        else if (dot[0] < 0 && dot[1] > 0) return 2;
+        else if (dot[0] < 0 && dot[1] < 0) return 3;
+        else return 4;
+    }
+}
