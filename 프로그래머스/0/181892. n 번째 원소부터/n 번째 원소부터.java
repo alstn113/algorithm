16bfe448b955cbcbdd445a3bc142bbc36fe541a3
김민수 / Minsu Kim <alstn113@gmail.com>
2024-06-26 09:43:02 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -2,10 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        List<Integer> result = new ArrayList<>();
-        for (int i = n-1; i<num_list.length; i++) {
-            result.add(num_list[i]);
-        }
-        return result.stream().mapToInt(i -> i).toArray();
+        return Arrays.copyOfRange(num_list, n-1, num_list.length);
     }
 }
