cfa4e21a3c2a502a3abaf39349b1b690e9d6a60c
김민수 / Minsu Kim <alstn113@gmail.com>
2024-06-27 13:09:09 +0900

    [level 0] Title: 접미사 배열, Time: 0.22 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -2,11 +2,13 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String my_string) {
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < my_string.length(); i++) {
-            result.add(my_string.substring(i));
+        String[] answer = new String[my_string.length()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = my_string.substring(i, my_string.length());
         }
-        result.sort(String::compareTo);
-        return result.toArray(new String[0]);
+
+        Arrays.sort(answer);
+        return answer;
     }
 }
