30b9ba326a2683983d8f5f1dc69bbb4494a1a531
Minsu Kim <alstn113@gmail.com>
2024-06-28 11:43:49 +0900

    [level 0] Title: 문자열 돌리기, Time: 150.60 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for (String s : a.split("")) {
+            System.out.println(s);
+        }
+    }
+}
