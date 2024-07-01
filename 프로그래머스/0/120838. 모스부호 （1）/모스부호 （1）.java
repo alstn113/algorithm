21e97d2bc5271234e490b47a6a7d8bb977ae824f
Minsu Kim <alstn113@gmail.com>
2024-07-01 11:02:22 +0900

    [level 0] Title: 모스부호 (1), Time: 0.20 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String letter) {
+        StringBuilder answer = new StringBuilder();
+        HashMap<String, String> morse = new HashMap<>();
+        morse.put(".-", "a");
+        morse.put("-...", "b");
+        morse.put("-.-.", "c");
+        morse.put("-..", "d");
+        morse.put(".", "e");
+        morse.put("..-.", "f");
+        morse.put("--.", "g");
+        morse.put("....", "h");
+        morse.put("..", "i");
+        morse.put(".---", "j");
+        morse.put("-.-", "k");
+        morse.put(".-..", "l");
+        morse.put("--", "m");
+        morse.put("-.", "n");
+        morse.put("---", "o");
+        morse.put(".--.", "p");
+        morse.put("--.-", "q");
+        morse.put(".-.", "r");
+        morse.put("...", "s");
+        morse.put("-", "t");
+        morse.put("..-", "u");
+        morse.put("...-", "v");
+        morse.put(".--", "w");
+        morse.put("-..-", "x");
+        morse.put("-.--", "y");
+        morse.put("--..", "z");
+        
+        String[] words = letter.split(" ");
+        for (String word : words) {
+            answer.append(morse.get(word));
+        }
+        return answer.toString();
+    }
+}
