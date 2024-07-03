39aec4a7eccc16817dde0b9102346cdde83161fe
Minsu Kim <alstn113@gmail.com>
2024-07-03 14:30:59 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 11.55 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,5 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        return my_string.substring(0, s) + overwrite_string + my_string.substring(s+overwrite_string.length());
+    }
+}
