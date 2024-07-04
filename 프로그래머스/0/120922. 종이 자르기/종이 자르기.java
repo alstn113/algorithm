5bae9d634d79cb1f7dca9ae7e25287584054e618
Minsu Kim <alstn113@gmail.com>
2024-07-04 21:51:30 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int M, int N) {
-        if (M > N) {
-            return (M-1)+(N-1)*M;            
-        } 
-        return (N-1)+(M-1)*N;            
+        return M - 1 + (N - 1 ) * M;        
     }
 }
