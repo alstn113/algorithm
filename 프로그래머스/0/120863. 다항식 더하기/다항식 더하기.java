907e2130880b1ffe700504b28a75855a89b2f047
Minsu Kim <alstn113@gmail.com>
2024-07-05 23:00:31 +0900

    [level 0] Title: 다항식 더하기, Time: 0.30 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String solution(String polynomial) {
+        String[] terms = polynomial.split(" \\+ ");
+        int xCoefficient = 0;
+        int constant = 0;
+
+        for (String term : terms) {
+            if (term.contains("x")) {
+                if (term.equals("x")) {
+                    xCoefficient += 1;
+                } else {
+                    xCoefficient += Integer.parseInt(term.replace("x", ""));
+                }
+            } else {
+                constant += Integer.parseInt(term);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        if (xCoefficient == 1) {
+            result.append("x");
+        } else if (xCoefficient > 1) {
+            result.append(xCoefficient).append("x");
+        }
+
+        if (constant > 0) {
+            if (result.length() > 0) {
+                result.append(" + ");
+            }
+            result.append(constant);
+        }
+
+        return result.toString();
+    }
+}
