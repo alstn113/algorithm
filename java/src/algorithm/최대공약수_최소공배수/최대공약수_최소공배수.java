b35e9d430464ac1e91cd18c9b34901023e69d63d
alstn113 <alstn113@gmail.com>
2025-02-10 22:43:47 +0900

    feat: 최대 공약수 최소 공배수

---
+++ java/src/algorithm/최대공약수_최소공배수/최대공약수_최소공배수.java
@@ -0,0 +1,31 @@
+package algorithm.최대공약수_최소공배수;
+
+public class 최대공약수_최소공배수 {
+
+    static final int N = 3;
+    static final int M = 12;
+
+    public static void main(String[] args) {
+        int gcd = gcd(N, M);
+        int lcm = lcm(N, M);
+
+        System.out.println("최대공약수: " + gcd);
+        System.out.println("최소공배수: " + lcm);
+    }
+
+    // 최대공약수
+    public static int gcd(int n, int m) {
+        if (m == 0) {
+            return n;
+        }
+        if (n > m) {
+            return gcd(m, n % m);
+        }
+        return gcd(n, m % n);
+    }
+
+    // 최소공배수
+    public static int lcm(int n, int m) {
+        return (n * m) / gcd(n, m);
+    }
+}
