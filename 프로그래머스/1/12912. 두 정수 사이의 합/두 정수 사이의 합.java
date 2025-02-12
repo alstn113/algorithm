bfc301936bef47a4db371cc32f81afb319888e99
Minsu Kim <alstn113@gmail.com>
2025-02-12 17:30:44 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 18.30 ms, Memory: 93.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        for (int i=a; i<=b; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
