75d0f82be1d2ff9a2235a557b9739780fdb9b416
Minsu Kim <alstn113@gmail.com>
2025-02-12 18:30:07 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+
+        while(num != 1){
+            if(answer==500) return -1;
+            
+            if(num%2==0) num/=2;
+            else num=num*3+1;
+            
+            answer += 1;
+        }
+        
+        return answer;
+    }
+}
