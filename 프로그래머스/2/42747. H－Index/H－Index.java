566a26883f4e60b9f92a1e107ce82bde45cfad0d
Minsu Kim <alstn113@gmail.com>
2025-02-20 10:13:40 +0900

    [level 2] Title: H-Index, Time: 0.88 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int len = citations.length;
+
+        for (int i = 0; i < len; i++) {
+            if (citations[i] >= len - i) {
+                return len - i;
+            }
+        }
+
+        return 0;
+    }
+}
+
