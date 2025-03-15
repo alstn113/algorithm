09555a93e5f8773abe2633d78c7b937f93186555
alstn113 <alstn113@gmail.com>
2025-03-15 19:31:08 +0900

    feat: 이진탐색 만족하는 최솟값 구하기

---
+++ java/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+class Solution {
+
+    static boolean[] visited = new boolean[9];
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java/src/algorithm/이진_탐색/만족하는_최솟값.java
@@ -0,0 +1,39 @@
+package algorithm.이진_탐색;
+
+// [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지
+// 출처: https://school.programmers.co.kr/learn/courses/30/lessons/340212
+
+// 숙련도가 높으면 통과, 낮으면 실패 -> 숙련도가 최소로 통과하는 값을 구하라
+public class 만족하는_최솟값 {
+
+    public int solution(int[] diffs, int[] times, long limit) {
+        int start = 1;
+        int end = 100000; // diffs 의 최댓값
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (calcTime(diffs, times, mid) <= limit) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start; // 통과하는 최소 숙련도
+    }
+
+    // 이거는 중요한 부분이 아닙니다.
+    public long calcTime(int[] diffs, int[] times, int level) {
+        long totalTime = 0;
+
+        for (int i = 0; i < diffs.length; i++) {
+            if (diffs[i] <= level) {
+                totalTime += times[i];
+            } else {
+                totalTime += (long) (times[i] + times[i - 1]) * (diffs[i] - level) + times[i];
+            }
+        }
+
+        return totalTime;
+    }
+}
