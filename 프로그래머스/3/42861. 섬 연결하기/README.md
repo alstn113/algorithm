f6383429ce679f10138e7e1abfc00d428e55606d
Minsu Kim <alstn113@gmail.com>
2025-03-19 12:42:57 +0900

    [level 3] Title: 섬 연결하기, Time: 1.13 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 78.4 MB, 시간: 14.40 ms
+메모리: 83.5 MB, 시간: 1.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 19일 11:42:03
+2025년 03월 19일 12:42:54
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,49 +1,46 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
+
+    static int[] parent;
+
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        int[] parent = new int[n];
-        for (int i=0; i<n; i++) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
-        
-        Queue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
-        for (int[] cost : costs) {
-            pq.offer(cost);
-        }
-        
-        while(!pq.isEmpty()) {
-            int[] node = pq.poll();
+
+        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
+
+        for (int[] node : costs) {
             int from = node[0];
             int to = node[1];
             int cost = node[2];
-            
-            if (findParent(from, parent) != findParent(to, parent)) {
-                System.out.println(from + " " + to + " " + cost);
-                unionParent(from, to, parent);
+
+            if (findParent(from) != findParent(to)) {
+                unionParent(from, to);
                 answer += cost;
             }
         }
-        
-        
+
         return answer;
     }
-    
-    public int findParent(int x, int[] parent) {
+
+    public int findParent(int x) {
         if (x == parent[x]) {
             return x;
         }
-        
-        return x = findParent(parent[x], parent);
+
+        return parent[x] = findParent(parent[x]);
     }
-    
-    public void unionParent(int a, int b, int[] parent) {
-        a = findParent(a, parent);
-        b = findParent(b, parent);
-        
+
+    public void unionParent(int a, int b) {
+        a = findParent(a);
+        b = findParent(b);
+
         if (a < b) {
-            parent[b] = a; 
+            parent[b] = a;
         } else {
             parent[a] = b;
         }
