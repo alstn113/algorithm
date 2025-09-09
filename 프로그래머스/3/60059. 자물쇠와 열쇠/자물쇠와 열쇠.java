861d97cec8e64f025895489d59d7189ecd639472
Minsu Kim <alstn113@gmail.com>
2025-09-09 23:09:42 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 5.44 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,34 +1,39 @@
 class Solution {
 
+    int M;
+    int N;
+
     public boolean solution(int[][] key, int[][] lock) {
-        int m = key.length;
-        int n = lock.length;
+        M = key.length;
+        N = lock.length;
 
         for (int i = 0; i < 4; i++) {
-            key = rotate(key, m);
+            key = rotate(key);
 
-            for (int x = -m + 1; x < n; x++) {
-                for (int y = -m + 1; y < n; y++) {
-                    if (check(key, lock, x, y, m, n)) {
+            for (int x = -M + 1; x < N; x++) {
+                for (int y = -M + 1; y < N; y++) {
+                    if (check(key, lock, x, y)) {
                         return true;
                     }
                 }
             }
         }
 
-        return false; 
+        return false;
     }
 
-    public boolean check(int[][] key, int[][] lock, int xOffset, int yOffset, int m, int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+    public boolean check(int[][] key, int[][] lock, int xOffset, int yOffset) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 int keyVal = 0;
                 int keyI = i - xOffset;
                 int keyJ = j - yOffset;
-                if (0 <= keyI && keyI < m && 0 <= keyJ && keyJ < m) {
+                
+                if (0 <= keyI && keyI < M && 0 <= keyJ && keyJ < M) {
                     keyVal = key[keyI][keyJ];
                 }
-                if (lock[i][j] + keyVal != 1) { 
+                
+                if (lock[i][j] + keyVal != 1) {
                     return false;
                 }
             }
@@ -36,12 +41,12 @@ class Solution {
         return true;
     }
 
-    public int[][] rotate(int[][] cur, int m) {
-        int[][] newKey = new int[m][m];
+    public int[][] rotate(int[][] cur) {
+        int[][] newKey = new int[M][M];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                newKey[j][m - 1 - i] = cur[i][j]; 
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < M; j++) {
+                newKey[j][M - 1 - i] = cur[i][j];
             }
         }
 
