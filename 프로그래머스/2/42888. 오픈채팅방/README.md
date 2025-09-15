bb06087d4b273f53a5e355bf252790dec16be24d
Minsu Kim <alstn113@gmail.com>
2025-09-15 12:23:36 +0900

    [level 2] Title: 오픈채팅방, Time: 457.05 ms, Memory: 334 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,14 +1,14 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
 
 ### 성능 요약
 
-메모리: 176 MB, 시간: 134.40 ms
+메모리: 334 MB, 시간: 457.05 ms
 
 ### 구분
 
-코딩테스트 연습 > 2019 KAKAO BLIND RECRUITMENT
+코딩테스트 연습 > 2019 KAKAO BLIND RECRUITMENT
 
 ### 채점결과
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 21일 15:59:07
+2025년 09월 15일 12:23:33
 
 ### 문제 설명
 
--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,4 +1,3 @@
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -6,42 +5,34 @@ import java.util.Map;
 
 class Solution {
     public String[] solution(String[] record) {
-        List<String[]> result = new ArrayList<>(); // command, uid
-        Map<String, String> nameMap = new HashMap<>();
+        Map<String, String> ids = new HashMap<>();
+        List<List<String>> result = new ArrayList<>();
 
         for (String r : record) {
             String[] split = r.split(" ");
-
-            String command = split[0];
-
-            if (command.equals("Enter")) {
-                String uid = split[1];
-                String name = split[2];
-
-                result.add(new String[]{command, uid, name});
-                nameMap.put(uid, name);
-            } else if (command.equals("Leave")) {
-                String uid = split[1];
-                result.add(new String[]{command, uid});
+            String action = split[0];
+            if (action.equals("Enter")) {
+                String id = split[1];
+                ids.put(id, split[2]);
+                result.add(List.of("Enter", id));
+            } else if (action.equals("Leave")) {
+                String id = split[1];
+                result.add(List.of("Leave", id));
             } else {
-                String uid = split[1];
+                String id = split[1];
                 String name = split[2];
-                nameMap.put(uid, name);
+                ids.put(id, name);
             }
         }
 
         String[] answer = new String[result.size()];
         for (int i = 0; i < result.size(); i++) {
-            String[] r = result.get(i);
-            String name = nameMap.get(r[1]);
-
-            if (r[0].equals("Enter")) {
-                answer[i] = name + "님이 들어왔습니다.";
+            if (result.get(i).get(0).equals("Enter")) {
+                answer[i] = String.format("%s님이 들어왔습니다.", ids.get(result.get(i).get(1)));
             } else {
-                answer[i] = name + "님이 나갔습니다.";
+                answer[i] = String.format("%s님이 나갔습니다.", ids.get(result.get(i).get(1)));
             }
         }
-
         return answer;
     }
 }
