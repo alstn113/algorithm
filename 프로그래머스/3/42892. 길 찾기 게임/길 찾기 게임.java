b9b5385de472c788b6ca661c4c8c8b7a2cb78a66
Minsu Kim <alstn113@gmail.com>
2025-09-26 00:45:21 +0900

    [level 3] Title: 길 찾기 게임, Time: 76.33 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,86 +1,80 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class Solution {
     public int[][] solution(int[][] nodeinfo) {
-        int n = nodeinfo.length;
-        Node[] nodes = new Node[n];
+        // 높이로 정렬
+        List<Node> arr = new ArrayList<>();
+        for (int i = 0; i < nodeinfo.length; i++) {
+            arr.add(new Node(i + 1, nodeinfo[i][0], nodeinfo[i][1], null, null));
+        }
+        arr.sort((o1, o2) -> o2.y - o1.y);
 
-        for (int i = 0; i < n; i++) {
-            nodes[i] = new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1);
+        Node root = arr.get(0);
+        for (int i = 1; i < arr.size(); i++) {
+            dfs(root, arr.get(i));
         }
 
-        Arrays.sort(nodes, (o1, o2) -> {
+        List<Integer> bfResult = new ArrayList<>();
+        bfIter(bfResult, root);
+        List<Integer> afResult = new ArrayList<>();
+        afIter(afResult, root);
 
-            return o2.y - o1.y;
-        });
-
-        Node root = nodes[0];
-
-        for (int i = 1; i < n; i++) {
-            insert(root, nodes[i]);
-        }
-
-        List<Integer> preorder = new ArrayList<>();
-        List<Integer> postorder = new ArrayList<>();
-
-        preorder(root, preorder);
-        postorder(root, postorder);
-
-        int[][] answer = new int[2][n];
-        for (int i = 0; i < n; i++) {
-            answer[0][i] = preorder.get(i);
-            answer[1][i] = postorder.get(i);
-        }
-        return answer;
+        return new int[][]{
+                bfResult.stream().mapToInt(v -> v).toArray(),
+                afResult.stream().mapToInt(v -> v).toArray()
+        };
     }
 
-    private void insert(Node parent, Node child) {
-        if (child.x < parent.x) {
-            if (parent.left == null) {
-                parent.left = child;
+    public void bfIter(List<Integer> bfResult, Node node) {
+        bfResult.add(node.num);
+        if (node.left != null) {
+            bfIter(bfResult, node.left);
+        }
+        if (node.right != null) {
+            bfIter(bfResult, node.right);
+        }
+    }
+
+    public void afIter(List<Integer> afResult, Node node) {
+        if (node.left != null) {
+            afIter(afResult, node.left);
+        }
+        if (node.right != null) {
+            afIter(afResult, node.right);
+        }
+        afResult.add(node.num);
+    }
+
+    public void dfs(Node target, Node cur) {
+        if (target.x < cur.x) {
+            if (target.right == null) {
+                target.right = cur;
             } else {
-                insert(parent.left, child);
+                dfs(target.right, cur);
             }
         } else {
-            if (parent.right == null) {
-                parent.right = child;
+            if (target.left == null) {
+                target.left = cur;
             } else {
-                insert(parent.right, child);
+                dfs(target.left, cur);
             }
         }
     }
 
-    private void preorder(Node node, List<Integer> result) {
-        if (node == null) {
-            return;
-        }
-        result.add(node.n);
-        preorder(node.left, result);
-        preorder(node.right, result);
-    }
-
-    private void postorder(Node node, List<Integer> result) {
-        if (node == null) {
-            return;
-        }
-        postorder(node.left, result);
-        postorder(node.right, result);
-        result.add(node.n);
-    }
-
-    static class Node {
+    class Node {
+        int num;
         int x;
         int y;
-        int n;
         Node left;
         Node right;
 
-        public Node(int x, int y, int n) {
+        public Node(int num, int x, int y, Node left, Node right) {
+            this.num = num;
             this.x = x;
             this.y = y;
-            this.n = n;
+            this.left = left;
+            this.right = right;
         }
     }
 }
