672ecfaecd5e871369b8d5616bff39e5b2be6987
Minsu Kim <alstn113@gmail.com>
2025-09-26 10:58:03 +0900

    [level 2] Title: 양궁대회, Time: 4.86 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,88 +1,77 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class Solution {
 
-    static int max = 0;
+    int max = 0;
+    List<List<Integer>> result = new ArrayList<>();
 
     public int[] solution(int n, int[] info) {
-        List<List<Integer>> result = new ArrayList<>();
-        dfs(info, result, new ArrayList<>(), n, 0);
+        // 어피치보다 점수가 높아야 함
+        // 그 중 최고 점수를 골라야 함.
+        // 최고 점수가 같은 것이 여러 개라면 최저 점수를 더 많이 맞춘 경우
+
+        int apeach = 0;
+        for (int i = 0; i < info.length; i++) {
+            if (info[i] > 0) {
+                apeach += 10 - i;
+            }
+        }
+
+        dfs(0, 0, apeach, new ArrayList<>(), n, info);
 
         if (result.isEmpty()) {
             return new int[]{-1};
         }
 
-        List<Integer> best = result.get(0);
-        for (int i = 1; i < result.size(); i++) {
-            if (compareScore(best, result.get(i))) {
-                best = result.get(i);
+        // 제일 낮은 점수가 오도록 정렬
+        result.sort((o1, o2) -> {
+            for (int i=10; i>-1; i--) {
+                if (o1.get(i).equals(o2.get(i))) {
+                    continue;
+                }
+                return o2.get(i) - o1.get(i);
             }
-        }
+            return 1;
+        });
 
-        return best.stream().mapToInt(i -> i).toArray();
+        return result.get(0).stream().mapToInt(i -> i).toArray();
     }
 
-    public void dfs(int[] info, List<List<Integer>> result, List<Integer> cur, int limit, int curTarget) {
-        if (curTarget == 10) {
-            cur.add(limit);
-            int diff = isWin(info, cur);
-            if (isWin(info, cur) > 0) {
-                if (diff > max) {
-                    max = diff;
-                    result.clear();
+    public void dfs(int idx, int lion, int apeach, List<Integer> cur, int remain, int[] info) {
+        if (idx == 11) {
+            if (remain > 0) {
+                cur.set(10, cur.get(10) + remain);
+            }
+
+            if (lion > apeach) {
+                if (lion - apeach > max) {
+                    max = lion - apeach;
+                    result = new ArrayList<>();
                     result.add(new ArrayList<>(cur));
-                } else if (diff == max) {
+                } else if (lion - apeach == max) {
                     result.add(new ArrayList<>(cur));
                 }
             }
-            cur.remove(cur.size() - 1);
             return;
         }
 
-        if (info[curTarget] < limit) {
-            int v = info[curTarget] + 1;
-            cur.add(v);
-            dfs(info, result, cur, limit - v, curTarget + 1);
+        if (info[idx] + 1 <= remain) {
+            int newLion = lion;
+            int newApeach = apeach;
+            if (info[idx] == 0) {
+                newLion += 10 - idx;
+            } else {
+                newLion += 10 - idx;
+                newApeach += -10 + idx;
+            }
+            cur.add(info[idx] + 1);
+            dfs(idx + 1, newLion, newApeach, cur, remain - info[idx] - 1, info);
             cur.remove(cur.size() - 1);
         }
 
         cur.add(0);
-        dfs(info, result, cur, limit, curTarget + 1);
+        dfs(idx + 1, lion, apeach, cur, remain, info);
         cur.remove(cur.size() - 1);
-    }
-
-    public int isWin(int[] info, List<Integer> cur) {
-        int a = 0;
-        int b = 0;
-
-        for (int i = 0; i < 10; i++) {
-            if (info[i] == 0 && cur.get(i) == 0) {
-                continue;
-            }
-
-            if (info[i] >= cur.get(i)) {
-                a += 10 - i;
-            } else {
-                b += 10 - i;
-            }
-        }
-
-        return b - a;
-    }
-
-    public boolean compareScore(List<Integer> best, List<Integer> candidate) {
-        for (int i = 10; i >= 0; i--) {
-            if (best.get(i) != candidate.get(i)) {
-                return candidate.get(i) > best.get(i);
-            }
-        }
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution(5, new int[]{2, 1, 1, 1, 0, 0, 0, 0, 0, 0})));
     }
 }
