ff7b3cc9dec90b43180d1daf235d05cf8dd49740
Minsu Kim <alstn113@gmail.com>
2025-09-28 21:45:12 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 8.98 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+
+    static int[] dx = {1, 0, 0, -1};
+    static int[] dy = {0, -1, 1, 0};
+    static String[] ds = {"d", "l", "r", "u"};
+
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        x = x - 1;
+        y = y - 1;
+        r = r - 1;
+        c = c - 1;
+
+        // d l r u 순으로 탐색
+        int dis = Math.abs(x - r) + Math.abs(y - c);
+        if (k < dis || (k - dis) % 2 != 0) {
+            return "impossible";
+        }
+
+        List<String> result = new ArrayList<>();
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(x, y, k, ""));
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            int cx = node.x;
+            int cy = node.y;
+            int cnt = node.cnt;
+            int newCnt = cnt - 1;
+
+            if (cnt == 0 && cx == r && cy == c) {
+                String s = node.s;
+                result.add(s);
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    continue;
+                }
+
+                int newDis = Math.abs(r - nx) + Math.abs(c - ny);
+                if (newCnt < newDis || (newCnt - newDis) % 2 != 0) {
+                    continue;
+                }
+
+                q.offer(new Node(nx, ny, newCnt, node.s + ds[i]));
+                break;
+            }
+        }
+
+        result.sort((o1, o2) -> o1.compareTo(o2));
+        return result.get(0);
+    }
+
+    class Node {
+        int x;
+        int y;
+        int cnt;
+        String s;
+
+        public Node(int x, int y, int cnt, String s) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+            this.s = s;
+        }
+    }
+}
