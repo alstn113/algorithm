16995ecd94b3b9c83373cbb9d07e319db75cbb7c
Minsu Kim <alstn113@gmail.com>
2025-10-02 12:26:20 +0900

    [level 2] Title: 순위 검색, Time: 2233.83 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/README.md
+++ 프로그래머스/2/72412. 순위 검색/README.md
@@ -1,10 +1,10 @@
 # [level 2] 순위 검색 - 72412 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72412?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72412) 
 
 ### 성능 요약
 
-메모리: 348 MB, 시간: 1417.88 ms
+메모리: 456 MB, 시간: 2233.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 10일 15:50:06
+2025년 10월 02일 12:26:17
 
 ### 문제 설명
 
--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,60 +10,59 @@ class Solution {
     public int[] solution(String[] info, String[] query) {
         for (String inf : info) {
             String[] split = inf.split(" ");
-            dfs(split, 0, new ArrayList<>());
+            dfs(split, new ArrayList<>());
         }
 
-        for (List<Integer> scores : map.values()) {
-            Collections.sort(scores);
+        for (Map.Entry<String, List<Integer>> entry : map.entrySet()) {
+            entry.getValue().sort((o1, o2) -> o1 - o2);
         }
 
-        int[] answer = new int[query.length];
+        int[] result = new int[query.length];
         for (int i = 0; i < query.length; i++) {
             String[] split = query[i].split(" and ");
-            String[] split2 = split[3].split(" ");
-            
-            String lang = split[0];
-            String job = split[1];
-            String career = split[2];
-            String food =  split2[0];
-            int score = Integer.parseInt(split2[1]);
-            String key = String.join(" ", Arrays.asList(lang, job, career, food));
+            String[] ssplit = split[3].split(" ");
+            String d = ssplit[0];
 
-            List<Integer> scores = map.getOrDefault(key, new ArrayList<>());
-            answer[i] = lowerBoundCount(scores, score);
-        }
+            String key = String.format("%s %s %s %s", split[0], split[1], split[2], d);
+            int score = Integer.parseInt(ssplit[1]);
 
-        return answer;
+            List<Integer> arr = map.getOrDefault(key, new ArrayList<>());
+            int left = 0;
+            int right = arr.size() - 1;
+            int tt = arr.size();
+
+            while( left <= right) {
+                int mid = (left  + right) / 2;
+
+                if ( arr.get(mid) >= score) {
+                    right = mid - 1;
+                    tt = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            result[i] = arr.size() - tt;
+         }
+
+        return result;
     }
 
-    private void dfs(String[] arr, int depth, List<String> key) {
-        if (depth == 4) {
-            String k = String.join(" ", key);
-            int score = Integer.parseInt(arr[4]);
-            map.computeIfAbsent(k, kk -> new ArrayList<>()).add(score);
+    public void dfs(String[] split, List<String> arr) {
+        if (arr.size() == 4) {
+            int score = Integer.parseInt(split[4]);
+            String key = String.join(" ", arr);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(score);
             return;
         }
 
-        key.add(arr[depth]);
-        dfs(arr, depth + 1, key);
-        key.remove(key.size() - 1);
+        int idx = arr.size();
+        arr.add(split[idx]);
+        dfs(split, arr);
+        arr.remove(arr.size() - 1);
 
-        key.add("-");
-        dfs(arr, depth + 1, key);
-        key.remove(key.size() - 1);
-    }
-
-    private int lowerBoundCount(List<Integer> scores, int target) {
-        int left = 0;
-        int right = scores.size();
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (scores.get(mid) < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return scores.size() - left;
+        arr.add("-");
+        dfs(split, arr);
+        arr.remove(arr.size() - 1);
     }
 }
