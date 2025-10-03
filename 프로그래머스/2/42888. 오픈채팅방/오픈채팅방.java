ae2e20388912836d2e70a09c61b676c1c29ca651
Minsu Kim <alstn113@gmail.com>
2025-10-04 08:55:25 +0900

    [level 2] Title: 오픈채팅방, Time: 461.45 ms, Memory: 308 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,32 +1,36 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
         Map<String, String> ids = new HashMap<>();
         List<List<String>> result = new ArrayList<>();
-
+        
         for (String r : record) {
             String[] split = r.split(" ");
+            
             String action = split[0];
+            String id = split[1];
+            
             if (action.equals("Enter")) {
-                String id = split[1];
-                ids.put(id, split[2]);
+                String name = split[2];
+                ids.put(id, name);                
                 result.add(List.of("Enter", id));
-            } else if (action.equals("Leave")) {
-                String id = split[1];
-                result.add(List.of("Leave", id));
-            } else {
-                String id = split[1];
+                continue;
+            }
+            
+            if (action.equals("Change")) {
                 String name = split[2];
                 ids.put(id, name);
+                continue;
+            }
+            
+            if (action.equals("Leave")) {
+                result.add(List.of("Leave", id));
             }
         }
 
         String[] answer = new String[result.size()];
-        for (int i = 0; i < result.size(); i++) {
+        for (int i=0; i<result.size(); i++) {
             if (result.get(i).get(0).equals("Enter")) {
                 answer[i] = String.format("%s님이 들어왔습니다.", ids.get(result.get(i).get(1)));
             } else {
