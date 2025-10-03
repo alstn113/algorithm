5a77a71c2d22f1550f009297503cd12bbe95896e
Minsu Kim <alstn113@gmail.com>
2025-10-03 11:22:39 +0900

    [level 3] Title: 기둥과 보 설치, Time: 29.12 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
@@ -1,96 +1,75 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 class Solution {
+
+    boolean[][][] frames;
+
     public int[][] solution(int n, int[][] build_frame) {
-        Set<List<Integer>> result = new HashSet<>();
+        frames = new boolean[n + 1][n + 1][2];
 
-        for (int[] bf : build_frame) {
-            int col = bf[0];
-            int row = bf[1];
-            int a = bf[2]; // 0은 기둥, 1은 보
-            int b = bf[3]; // 0은 삭제, 1은 설치
+        for (int[] frame : build_frame) {
+            int x = frame[0];
+            int y = frame[1];
+            int type = frame[2];
+            int cd = frame[3];
 
-            if (b == 1) {
-                if (a == 0) {
-                    // 기둥 설치
-                    if (row == 0 // 바닥
-                            || result.contains(List.of(col - 1, row, 1)) // 왼쪽 보
-                            || result.contains(List.of(col, row, 1)) // 오른쪽 보
-                            || result.contains(List.of(col, row - 1, 0)) // 아래 기둥
-                    ) {
-                        result.add(List.of(col, row, a));
+            if (cd == 1) {
+                if (type == 0) {
+                    if (canBuildGi(x, y)) {
+                        frames[y][x][0] = true;
                     }
                 } else {
-                    // 보 설치
-                    if (result.contains(List.of(col, row - 1, 0)) // 왼쪽 아래 기둥
-                            || result.contains(List.of(col + 1, row - 1, 0)) // 오른쪽 아래 기둥
-                            || (result.contains(List.of(col - 1, row, 1)) && result.contains(List.of(col + 1, row, 1)))
-                    ) {
-                        result.add(List.of(col, row, a));
+                    if (canBuildBo(x, y, n)) {
+                        frames[y][x][1] = true;
                     }
                 }
-            } else {
-                // 1. 일단 삭제 처리
-                // 연결된 구조들이 문제가 없는지 확인
-                // 이것과 연결된 구조들이 문제가 없는지 확인
-                // 하나라도 문제가 있으면 복구 처리
-                // 이것을 재귀적으로 처리
-                result.remove(List.of(col, row, a));
-                if (!isPossible(result)) {
-                    result.add(List.of(col, row, a));
+            }
+            else {
+                frames[y][x][type] = false;
+                if (!canBuild(n)) {
+                    frames[y][x][type] = true;
                 }
             }
         }
-        
-        List<List<Integer>> answer= new ArrayList<>();
-        for (List<Integer> structure : result) {
-            answer.add(structure);
-        }
-        answer.sort((o1, o2) -> {
-            if (o1.get(0).equals(o2.get(0))) {
-                if (o1.get(1).equals(o2.get(1))) {
-                    return o1.get(2) - o2.get(2);
-                }
-                return o1.get(1) - o2.get(1);
-            }
-            return o1.get(0) - o2.get(0);
-        });
-        int[][] answerArr = new int[answer.size()][3];
-        for (int i = 0; i < answer.size(); i++) {
-            answerArr[i][0] = answer.get(i).get(0);
-            answerArr[i][1] = answer.get(i).get(1);
-            answerArr[i][2] = answer.get(i).get(2);
-        }
-        return answerArr;
-    }
-    
-    public boolean isPossible(Set<List<Integer>> result) {
-        for (List<Integer> structure : result) {
-            int col = structure.get(0);
-            int row = structure.get(1);
-            int a = structure.get(2); 
 
-            if (a == 0) {
-              
-                if (!(row == 0 
-                        || result.contains(List.of(col - 1, row, 1)) 
-                        || result.contains(List.of(col, row, 1)) 
-                        || result.contains(List.of(col, row - 1, 0)) 
-                )) {
+        List<int[]> result = new ArrayList<>();
+        for (int x = 0; x <= n; x++) {
+            for (int y = 0; y <= n; y++) {
+                for (int t = 0; t < 2; t++) {
+                    if (frames[y][x][t]) {
+                        result.add(new int[]{x, y, t});
+                    }
+                }
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public boolean canBuild(int n) {
+        for (int y = 0; y <= n; y++) {
+            for (int x = 0; x <= n; x++) {
+                if (frames[y][x][0] && !canBuildGi(x, y)) {
                     return false;
                 }
-            } else {
-                if (!(result.contains(List.of(col, row - 1, 0)) 
-                        || result.contains(List.of(col + 1, row - 1, 0)) 
-                        || (result.contains(List.of(col - 1, row, 1)) && result.contains(List.of(col + 1, row, 1)))
-                )) {
+                if (frames[y][x][1] && !canBuildBo(x, y, n)) {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    public boolean canBuildGi(int x, int y) {
+        return y == 0
+                || (y > 0 && frames[y - 1][x][0])
+                || (x > 0 && frames[y][x - 1][1])
+                || frames[y][x][1];
+    }
+
+    public boolean canBuildBo(int x, int y, int n) {
+        return (y > 0 && (frames[y - 1][x][0] || frames[y - 1][x + 1][0])) // 한쪽 끝이 기둥 위
+                || (x > 0 && x < n && frames[y][x - 1][1] && frames[y][x + 1][1]); // 양쪽 보 연결
     }
 }
