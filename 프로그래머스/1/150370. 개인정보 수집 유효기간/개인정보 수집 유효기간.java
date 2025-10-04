6ec65850c441536da0e83dd770ab9e6b1b7eee3a
Minsu Kim <alstn113@gmail.com>
2025-10-04 18:16:09 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.01 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,36 +1,34 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        int todate = getDate(today);
+        int todayDate = getDate(today);
         Map<String, Integer> map = new HashMap<>();
-        for (String t : terms) {
-            String[] split = t.split(" ");
-            map.put(split[0], Integer.parseInt(split[1]));
+        for (String term : terms) {
+            String[] split = term.split(" ");
+            map.put(split[0], Integer.parseInt(split[1]) * 28);
         }
-
+        
         List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < privacies.length; i++) {
+        for (int i=0; i<privacies.length; i++) {
             String[] split = privacies[i].split(" ");
             int date = getDate(split[0]);
-            String term = split[1];
-            int termM = map.get(term);
-
-            if (date + termM * 28 <= todate) {
-                result.add(i + 1);
+            int termDate  = map.get(split[1]);
+            
+            if (date + termDate - 1 < todayDate) {
+                result.add(i+1);
             }
         }
-        return result.stream().mapToInt(v -> v).toArray();
+        
+        return result.stream().mapToInt(i -> i).toArray();
     }
-
+    
     public int getDate(String date) {
         String[] split = date.split("\\.");
         int y = Integer.parseInt(split[0]) * 12 * 28;
         int m = Integer.parseInt(split[1]) * 28;
         int d = Integer.parseInt(split[2]);
+        
         return y + m + d;
     }
 }
