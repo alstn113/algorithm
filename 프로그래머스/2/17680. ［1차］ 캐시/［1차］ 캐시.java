822af282c8582b7fb8dfce9b3e62e08245e6f388
Minsu Kim <alstn113@gmail.com>
2025-10-04 09:09:46 +0900

    [level 2] Title: [1차] 캐시, Time: 37.95 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,29 +1,28 @@
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
-
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+        
         Queue<String> q = new LinkedList<>();
-        for (String city : cities) {
-            city = city.toLowerCase();
-            if (q.contains(city)) {
-                q.remove(city);
-                q.offer(city);
-                answer += 1;
+        int result = 0;
+        
+        for (int i=0; i<cities.length; i++) {
+            String target = cities[i].toUpperCase();
+            if (q.contains(target)) {
+                q.remove(target);
+                q.offer(target);
+                result += 1;
             } else {
-                if (q.size() < cacheSize) {
-                    q.offer(city);
-                } else if (cacheSize > 0) {
+                if (q.size() == cacheSize) {
                     q.poll();
-                    q.offer(city);
                 }
-                answer += 5;
+                q.offer(target);
+                result += 5;
             }
         }
-
-        return answer;
+        return result;
     }
 }
