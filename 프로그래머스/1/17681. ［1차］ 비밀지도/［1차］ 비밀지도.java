7b0f891d94f741a58c18761573b7568f3c0879dc
Minsu Kim <alstn113@gmail.com>
2025-10-04 09:21:38 +0900

    [level 1] Title: [1차] 비밀지도, Time: 4.16 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,27 +1,19 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] result = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            String v = Integer.toBinaryString(arr1[i] | arr2[i]);
-            StringBuilder binary = new StringBuilder(v);
-
-            while (binary.length() < n) {
-                binary.insert(0, "0");
+        
+        for (int i=0; i<n; i++) {
+            String s = Integer.toBinaryString(arr1[i] | arr2[i]);
+            
+            while(s.length() < n) {
+                s = " " + s;
             }
-
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                if (binary.charAt(j) == '0') {
-                    sb.append(" ");
-                } else {
-                    sb.append("#");
-                }
-            }
-
-            result[i] = sb.toString();
+            
+            s = s.replaceAll("1", "#");
+            s = s.replaceAll("0", " ");
+            result[i] = s;
         }
-
+        
         return result;
     }
 }
