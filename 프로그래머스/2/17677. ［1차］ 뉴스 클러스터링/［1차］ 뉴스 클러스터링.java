5a52fae6a1c461563197485a900fd18116ac3653
Minsu Kim <alstn113@gmail.com>
2025-10-04 14:31:58 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 19.53 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,44 +1,59 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public int solution(String str1, String str2) {
         Map<String, Integer> map1 = new HashMap<>();
         Map<String, Integer> map2 = new HashMap<>();
+        setMap(str1, map1);
+        setMap(str2, map2);
 
-        createMap(str1, map1);
-        createMap(str2, map2);
-
-        int intersection = 0;
+        int inter = 0;
         int sum = 0;
 
         for (Map.Entry<String, Integer> entry : map1.entrySet()) {
-            sum += entry.getValue();
-            intersection += Math.min(entry.getValue(), map2.getOrDefault(entry.getKey(), 0));
-        }
-        for (Map.Entry<String, Integer> entry : map2.entrySet()) {
-            sum += entry.getValue();
-        }
-        sum -= intersection;
+            String key = entry.getKey();
+            int value = entry.getValue();
 
-        if (intersection == 0 && sum == 0) {
+            if (map2.containsKey(key)) {
+                int map2Value = map2.get(key);
+                sum += Math.max(value, map2Value);
+                inter += Math.min(value, map2Value);
+            } else {
+                sum += value;
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : map2.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
+
+            if (!map1.containsKey(key)) {
+                sum += value;
+            }
+        }
+        
+        if (sum == 0) {
             return 65536;
         }
 
-        return (int) ((double) intersection / sum * 65536);
+        return 65536 * inter / sum;
     }
 
-    public void createMap(String str, Map<String, Integer> map) {
-        for (int i = 0; i < str.length() - 1; i++) {
-            if (!Character.isAlphabetic(str.charAt(i))) {
+    public void setMap(String str, Map<String, Integer> map) {
+        for (int i=0; i<str.length()-1; i++) {
+            char c1 = str.charAt(i);
+            char c2 = str.charAt(i+1);
+            if(!Character.isAlphabetic(c1)) {
                 continue;
             }
-            if (!Character.isAlphabetic(str.charAt(i + 1))) {
+            if(!Character.isAlphabetic(c2)) {
                 i += 1;
                 continue;
             }
-            String target = str.substring(i, i + 2).toLowerCase();
-            map.put(target, map.getOrDefault(target, 0) + 1);
+
+            String s = "" + c1 + c2;
+            s = s.toUpperCase();
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
     }
 }
