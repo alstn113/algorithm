efeed1316b7ae28f754ef4d9886670637ae97be2
Minsu Kim <alstn113@gmail.com>
2025-10-05 10:43:36 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 9.96 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,60 +1,57 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] files) {
         List<File> result = new ArrayList<>();
+        
         for (String file : files) {
-            int n = file.length();
-            int i = 0;
-            StringBuilder sb = new StringBuilder();
-
-            while (true) {
-                char target = file.charAt(i);
-                if (!Character.isDigit(target)) {
-                    sb.append(target);
-                    i += 1;
+            int idx = 0;
+            
+            while(true) {
+                char c = file.charAt(idx);
+                if (!Character.isDigit(c)) {
+                    idx += 1;
                 } else {
                     break;
                 }
             }
-
-            String head = sb.toString().toUpperCase();
-
-            int k = 5;
-            StringBuilder sb1 = new StringBuilder();
-            while (k > 0 && i < n) {
-                char target = file.charAt(i);
-                if (Character.isDigit(target)) {
-                    sb1.append(target);
-                    i += 1;
-                    k -= 1;
+            
+            int tmpIdx = idx;
+            String head = file.substring(0, idx).toUpperCase();
+            
+            int limit = 5;
+            while (limit > 0 && idx < file.length()) {
+                char c = file.charAt(idx);
+                if (Character.isDigit(c)) {
+                    idx += 1;
+                    limit -= 1;
                 } else {
                     break;
                 }
             }
-            int number = Integer.parseInt(sb1.toString());
-
-            result.add(new File(file, head, number));
-        }
-
+            int number = Integer.parseInt(file.substring(tmpIdx, idx));
+            
+            File newFile = new File(file, head, number);
+            result.add(newFile);
+        }        
+        
         result.sort((o1, o2) -> {
             if (o1.head.equals(o2.head)) {
                 return o1.number - o2.number;
             }
             return o1.head.compareTo(o2.head);
         });
-
-        return result.stream().map(file -> file.name).toArray(String[]::new);
+        
+        return result.stream().map(v -> v.real).toArray(String[]::new);
     }
-
+    
     class File {
-        String name;
-        String head; // 대소문자 x
-        int number; // 숫자로
-
-        public File(String name, String head, int number) {
-            this.name = name;
+        String real;
+        String head;
+        int number;
+        
+        public File(String real, String head, int number) {
+            this.real = real;
             this.head = head;
             this.number = number;
         }
