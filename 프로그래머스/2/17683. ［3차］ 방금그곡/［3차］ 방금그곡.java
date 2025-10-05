2b944559f8b2b9fc4dfb3128394750ee6134861e
Minsu Kim <alstn113@gmail.com>
2025-10-05 11:14:19 +0900

    [level 2] Title: [3차] 방금그곡, Time: 11.74 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,52 +1,63 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public String solution(String m, String[] musicinfos) {
+        // 음악 길이 < 재생 시간 -> 반복
+        // 음악 길이 > 재생 시간 -> 처음부터 재생 시간 만큼
+
+        // 일치 음악 여러 개 -> 재생 시간 제일 긴 것 -> 먼저 입력
+
         List<Music> result = new ArrayList<>();
 
         m = convert(m);
 
-        for (String info : musicinfos) {
-            String[] split = info.split(",");
+        for (String ms : musicinfos) {
+            String[] split = ms.split(",");
             int time = getTime(split[1]) - getTime(split[0]);
-            String name = split[2];
+            String title = split[2];
             String melody = convert(split[3]);
+
+            String play = melody;
             if (melody.length() < time) {
-                melody = melody.repeat(time / melody.length() + 1);
-                melody = melody.substring(0, time);
+                play = melody.repeat(time / melody.length() + 1);
+                play = play.substring(0, time);
             } else if (melody.length() > time) {
-                melody = melody.substring(0, time);
+                play = melody.substring(0, time);
             }
 
-            if (melody.contains(m)) {
-                result.add(new Music(time, name));
+            if (play.contains(m)) {
+                result.add(new Music(play.length(), title));
             }
+        }
+
+        if(result.isEmpty()) {
+            return "(None)";
         }
 
         result.sort((o1, o2) -> {
-            return o2.time - o1.time;
+            return o2.pt - o1.pt;
         });
 
-        if (result.isEmpty()) {
-            return "(None)";
-        }
-        return result.get(0).name;
+
+        return result.get(0).title;
     }
 
     class Music {
-        int time;
-        String name;
+        int pt;
+        String title;
 
-        public Music(int time, String name) {
-            this.time = time;
-            this.name = name;
+        public Music(int pt, String title) {
+            this.pt = pt;
+            this.title = title;
         }
     }
 
     public int getTime(String time) {
         String[] split = time.split(":");
-        return Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
+        int h = Integer.parseInt(split[0]) * 60;
+        int m = Integer.parseInt(split[1]);
+
+        return h + m;
     }
 
     public String convert(String str) {
@@ -61,4 +72,3 @@ class Solution {
         return str;
     }
 }
-
