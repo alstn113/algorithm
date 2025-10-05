292bd8b5eddd3f8f7f5dee0b4fab82864323c866
Minsu Kim <alstn113@gmail.com>
2025-10-05 12:53:17 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.07 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,52 +1,45 @@
 class Solution {
     public int solution(String dartResult) {
-        char[] chars = dartResult.toCharArray();
-        int n = dartResult.length();
-        int i = 0;
-        int k = 0;
         int[] result = new int[3];
-        while (i < n) {
-            // 1
-            int v;
-            if (chars[i] == '1' && chars[i + 1] == '0') {
-                v = 10;
+        
+        int i = 0;
+        for (int k=0; k<3; k++) {
+            // 첫 번째
+            char c = dartResult.charAt(i);
+            char nc = dartResult.charAt(i+1);
+            if (c == '1' && nc == '0') {
+                result[k] = 10;                      
                 i += 1;
             } else {
-                v = Integer.parseInt(dartResult.substring(i, i + 1));
+                result[k] = Integer.parseInt(String.valueOf(c));                
             }
-            result[k] = v;
             i += 1;
-
-            // 2
-            char bonus = chars[i];
-            if (bonus == 'D') {
+            
+            // 두 번째
+            char oc = dartResult.charAt(i);
+            if (oc == 'D') {
                 result[k] = result[k] * result[k];
-            } else if (bonus == 'T') {
+            } else if (oc == 'T') {
                 result[k] = result[k] * result[k] * result[k];
             }
             i += 1;
-
-            // 3
-            if (i == n) {
-                break;
-            }
-            if (Character.isDigit(chars[i])) {
-                k += 1;
-                continue;
-            }
-            char option = chars[i];
-            if (option == '*') {
-                result[k] *= 2;
-                if (k != 0) {
-                    result[k - 1] *= 2;
+            
+            // 세 번째
+            if (i < dartResult.length()) {
+                if (dartResult.charAt(i) == '*') {
+                    result[k] *= 2;
+                    if (k > 0) {
+                        result[k-1] *= 2;
+                    }
+                    i += 1;
+                } else if (dartResult.charAt(i) == '#') {
+                    result[k] *= -1;
+                    i += 1;
                 }
-            } else if (option == '#') {
-                result[k] *= -1;
             }
-            i+=1;
-            k += 1;
         }
-
+        
+        
         return result[0] + result[1] + result[2];
     }
 }
