f03f2b35553d08bccb8a6495e5f1d58703572bfd
Minsu Kim <alstn113@gmail.com>
2025-10-06 17:42:17 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 92.37 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,47 +1,45 @@
 class Solution {
     public int[] solution(long[] numbers) {
-        int[] ff = new int[]{1, 3, 7, 15, 31, 63};
         int[] result = new int[numbers.length];
-
-        for (int i = 0; i < numbers.length; i++) {
-            long number = numbers[i];
-            String bin = Long.toString(number, 2);
-            int binLen = bin.length();
-            for (int k = 0; k < ff.length; k++) {
-                if (binLen == ff[k]) {
+        
+        for (int i=0; i<numbers.length; i++) {
+            String v = Long.toString(numbers[i], 2);
+            int len = v.length();
+            int[] arr = new int[] {1, 3, 7, 15, 31, 63};
+            for (int a : arr) {
+                if (len <= a) {
+                    v = "0".repeat(a - len) + v;
                     break;
-                }
-                if (binLen < ff[k]) {
-                    bin = "0".repeat(ff[k] - binLen) + bin;
-                    break;
-                }
+                }         
             }
-
-            if (dfs(bin, true)) {
+            
+            // 가능하면 true; 불가능하면 false;
+            if(dfs(v, true)) {
                 result[i] = 1;
             }
-        }
+        }   
+         
         return result;
     }
-
-    public boolean dfs(String target, boolean prevOne) {
-        if (target.length() == 1) {
-            if(!prevOne && target.equals("1")) {
+    
+    public boolean dfs(String v, boolean prevOne) {
+        int len = v.length();
+        if (len == 1) {
+            if(!prevOne && v.equals("1")) {
                 return false;
             }
             return true;
         }
+        
+        String left = v.substring(0, len / 2);
+        char mid = v.charAt(len / 2);
+        String right = v.substring(len / 2 + 1);
 
-        int len = target.length() / 2;
-        String left = target.substring(0, len);
-        char mid = target.charAt(len);
-        String right = target.substring(len + 1);
-
-        boolean isMidOne = mid == '1';
-        if (!prevOne && isMidOne) {
+        boolean prev = mid == '1';
+        if (!prevOne && prev) {
             return false;
         }
-
-        return dfs(left, isMidOne) && dfs(right, isMidOne);
+        
+        return dfs(left, prev) && dfs(right, prev);
     }
-} 
+}
