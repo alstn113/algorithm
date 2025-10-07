bac929b1c577937aeb5f8e43fc7c4d6308d48fcc
Minsu Kim <alstn113@gmail.com>
2025-10-07 11:31:50 +0900

    [level 2] Title: 양궁대회, Time: 9.64 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,77 +1,75 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-
-    int max = 0;
+    
     List<List<Integer>> result = new ArrayList<>();
-
+    int max = 1;
+    
     public int[] solution(int n, int[] info) {
-        // 어피치보다 점수가 높아야 함
-        // 그 중 최고 점수를 골라야 함.
-        // 최고 점수가 같은 것이 여러 개라면 최저 점수를 더 많이 맞춘 경우
-
-        int apeach = 0;
-        for (int i = 0; i < info.length; i++) {
-            if (info[i] > 0) {
-                apeach += 10 - i;
-            }
-        }
-
-        dfs(0, 0, apeach, new ArrayList<>(), n, info);
-
+        // 같은 횟수로 맞춤 -> 어피치 점수, 아무도 못 맞춤 -> 둘 다 0점
+        // 여러 경우 중 가장 큰 점수 차이로 우승 -> 다음 정렬로 가장 낮은 점수를 더 많이 맞힌 경우
+        
+        dfs(n, info, new ArrayList<>());
+        
         if (result.isEmpty()) {
-            return new int[]{-1};
+            return new int[] {-1};
         }
-
-        // 제일 낮은 점수가 오도록 정렬
+        
         result.sort((o1, o2) -> {
-            for (int i=10; i>-1; i--) {
-                if (o1.get(i).equals(o2.get(i))) {
-                    continue;
-                }
-                return o2.get(i) - o1.get(i);
-            }
-            return 1;
+           for (int i=10; i>-1; i--) {
+               if (!o2.get(i).equals(o1.get(i))) {
+                   return o2.get(i) - o1.get(i);
+               }
+           }    
+            return 0;
         });
-
-        return result.get(0).stream().mapToInt(i -> i).toArray();
+        
+        return result.get(0).stream().mapToInt(i-> i).toArray();
     }
-
-    public void dfs(int idx, int lion, int apeach, List<Integer> cur, int remain, int[] info) {
-        if (idx == 11) {
+    
+    public void dfs(int remain, int[] info, List<Integer> cur) {
+        if (cur.size() == 11) {
             if (remain > 0) {
                 cur.set(10, cur.get(10) + remain);
             }
-
-            if (lion > apeach) {
-                if (lion - apeach > max) {
-                    max = lion - apeach;
-                    result = new ArrayList<>();
-                    result.add(new ArrayList<>(cur));
-                } else if (lion - apeach == max) {
-                    result.add(new ArrayList<>(cur));
+            int aSum = 0;
+            int bSum = 0;
+            
+            for (int i=0; i<11; i++) {
+                if (info[i] == 0 && cur.get(i) == 0) {
+                    continue;
+                }
+                
+                if (info[i] >= cur.get(i)) {
+                    aSum += 10-i;
+                } else {
+                    bSum += 10-i;
                 }
             }
+            
+            int diff = bSum - aSum;
+            if (diff == 0) return;
+            if (diff == max) {
+                result.add(new ArrayList<>(cur));
+            } else if (diff > max) {
+                max = diff;
+                result = new ArrayList<>();
+                result.add(new ArrayList<>(cur));
+            } 
+            
             return;
         }
-
-        if (info[idx] + 1 <= remain) {
-            int newLion = lion;
-            int newApeach = apeach;
-            if (info[idx] == 0) {
-                newLion += 10 - idx;
-            } else {
-                newLion += 10 - idx;
-                newApeach += -10 + idx;
-            }
+        
+        int idx = cur.size();
+        if (info[idx] < remain) {
             cur.add(info[idx] + 1);
-            dfs(idx + 1, newLion, newApeach, cur, remain - info[idx] - 1, info);
-            cur.remove(cur.size() - 1);
+            int newRemain = remain - (info[idx]+1);
+            dfs(newRemain, info, new ArrayList<>(cur));
+            cur.remove(cur.size()-1);
         }
-
+        
         cur.add(0);
-        dfs(idx + 1, lion, apeach, cur, remain, info);
-        cur.remove(cur.size() - 1);
+        dfs(remain, info, new ArrayList<>(cur));
+        cur.remove(cur.size()-1);
     }
 }
