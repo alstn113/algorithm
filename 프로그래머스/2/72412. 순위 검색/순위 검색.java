dbd195c9a9df675a6ea44151f8541e7e0c3dc624
Minsu Kim <alstn113@gmail.com>
2025-10-07 09:40:54 +0900

    [level 2] Title: 순위 검색, Time: 2366.81 ms, Memory: 468 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,68 +1,71 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-
-    Map<String, List<Integer>> map = new HashMap<>();
-
     public int[] solution(String[] info, String[] query) {
+        Map<String, List<Integer>> map = new HashMap<>();
         for (String inf : info) {
             String[] split = inf.split(" ");
-            dfs(split, new ArrayList<>());
+            String lan = split[0];
+            String type = split[1];
+            String year = split[2];
+            String food = split[3];
+            String[] arr = new String[] {lan, type, year, food};
+            int score = Integer.parseInt(split[4]);
+
+            dfs(arr, score, new ArrayList<>(), map);
         }
 
         for (Map.Entry<String, List<Integer>> entry : map.entrySet()) {
-            entry.getValue().sort((o1, o2) -> o1 - o2);
+            Collections.sort(entry.getValue());
         }
 
         int[] result = new int[query.length];
-        for (int i = 0; i < query.length; i++) {
+        for (int i=0; i<query.length; i++) {
             String[] split = query[i].split(" and ");
+            String lan = split[0];
+            String type = split[1];
+            String year = split[2];
             String[] ssplit = split[3].split(" ");
-            String d = ssplit[0];
-
-            String key = String.format("%s %s %s %s", split[0], split[1], split[2], d);
+            String food = ssplit[0];
             int score = Integer.parseInt(ssplit[1]);
 
-            List<Integer> arr = map.getOrDefault(key, new ArrayList<>());
+            String key = String.format("%s %s %s %s", lan, type, year, food);
+            List<Integer> target = map.getOrDefault(key, new ArrayList<>());
+
             int left = 0;
-            int right = arr.size() - 1;
-            int tt = arr.size();
+            int right = target.size() - 1;
+            int min = 0;
 
-            while( left <= right) {
-                int mid = (left  + right) / 2;
-
-                if ( arr.get(mid) >= score) {
+            while(left <= right) {
+                int mid = (left + right) / 2;
+                if (score <= target.get(mid)) {
                     right = mid - 1;
-                    tt = mid;
                 } else {
                     left = mid + 1;
+                    min = left;
                 }
             }
 
-            result[i] = arr.size() - tt;
-         }
-
+            result[i] = target.size() - min;
+        }
         return result;
     }
 
-    public void dfs(String[] split, List<String> arr) {
-        if (arr.size() == 4) {
-            int score = Integer.parseInt(split[4]);
-            String key = String.join(" ", arr);
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(score);
+    public void dfs(String[] arr, int score, List<String> cur, Map<String, List<Integer>> map) {
+        if (cur.size() == 4) {
+            String key = String.join(" ", cur);
+            List<Integer> mapList = map.computeIfAbsent(key, k -> new ArrayList<>());
+            mapList.add(score);
             return;
         }
 
-        int idx = arr.size();
-        arr.add(split[idx]);
-        dfs(split, arr);
-        arr.remove(arr.size() - 1);
+        int idx = cur.size();
+        cur.add(arr[idx]);
+        dfs(arr, score, cur, map);
+        cur.remove(cur.size() - 1);
 
-        arr.add("-");
-        dfs(split, arr);
-        arr.remove(arr.size() - 1);
+        cur.add("-");
+        dfs(arr, score, new ArrayList<>(cur), map);
+        cur.remove(cur.size()-1);
     }
 }
