c5f19029fdc8da35ed6b8a784f4f216a8a59c65d
Minsu Kim <alstn113@gmail.com>
2025-10-08 19:08:37 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 52.35 ms, Memory: 213 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -2,40 +2,45 @@ class Solution {
     public int solution(int[][] board, int[][] skill) {
         int n = board.length;
         int m = board[0].length;
-        int[][] temp = new int[n + 1][m + 1];
-        for (int[] s : skill) {
-            int degree = s[5];
-            if (s[0] == 1) {
-                degree *= -1;
-            }
-
-            temp[s[1]][s[2]] += degree;
-            temp[s[1]][s[4] + 1] -= degree;
-            temp[s[3] + 1][s[2]] -= degree;
-            temp[s[3] + 1][s[4] + 1] += degree;
+        
+        int[][] nb = new int[n+1][m+1];
+        
+        for (int[] sk : skill) {
+            int type = sk[0]; // type=1 : 감소, type=2 : 증가
+            int r1 = sk[1];
+            int c1 = sk[2];
+            int r2 = sk[3];
+            int c2 = sk[4];
+            int degree = sk[5];
+            
+            int v = type == 1 ? -1 : 1;
+            nb[r1][c1] += v * degree;
+            nb[r1][c2+1] += -1 * v * degree;
+            nb[r2+1][c1] += -1 * v * degree;
+            nb[r2+1][c2+1] += v * degree;
         }
-
-        for (int j = 0; j < m; j++) {
-            for (int i = 1; i < n; i++) {
-                temp[i][j] += temp[i - 1][j];
+        
+        for (int i=0; i<n; i++) {
+            for (int j=1; j<m; j++) {
+                nb[i][j] += nb[i][j-1];
             }
         }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                temp[i][j] += temp[i][j - 1];
+        
+        for (int i=0; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                nb[j][i] += nb[j-1][i];
             }
         }
-
+        
         int result = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] + temp[i][j] >= 1) {
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (board[i][j] + nb[i][j] >= 1) {
                     result += 1;
                 }
             }
         }
-
+        
         return result;
     }
 }
