b612f66266b300e240ee0150d92bb99851da38b5
Minsu Kim <alstn113@gmail.com>
2025-10-08 10:49:04 +0900

    [level 3] Title: 양과 늑대, Time: 4.21 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
 
 ### 성능 요약
 
-메모리: 87.7 MB, 시간: 6.70 ms
+메모리: 90 MB, 시간: 4.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 24일 12:27:29
+2025년 10월 08일 10:49:02
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,48 +1,42 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    int result = 0;
 
+    List<List<Integer>> graph = new ArrayList<>();
+    int max = 1;
+    
     public int solution(int[] info, int[][] edges) {
         int n = info.length;
-
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i=0; i<n; i++) {
             graph.add(new ArrayList<>());
         }
-
         for (int[] edge : edges) {
-            int a = edge[0];
-            int b = edge[1];
-
-            graph.get(a).add(b);
+            int from = edge[0];
+            int to = edge[1];
+            graph.get(from).add(to);
         }
-
-        List<Integer> initial = new ArrayList<>();
-        initial.add(0);
-        dfs(graph, info, initial, 0, 0);
-
-        return result;
+        
+        dfs(1, 0, new ArrayList<>(graph.get(0)), info);
+        
+        return max;
     }
-
-    public void dfs(List<List<Integer>> graph, int[] info, List<Integer> able, int sheep, int wolf) {
-        for (Integer target : new ArrayList<>(able)) {
-            int newSheep = info[target] == 0 ? sheep + 1 : sheep;
-            int newWolf = info[target] == 1 ? wolf + 1 : wolf;
-
-            if (newWolf >= newSheep) {
+    
+    public void dfs(int s, int w, List<Integer> nodes, int[] info) {
+        for (int node : nodes) {
+            int newSheep = info[node] == 0  ? 1 : 0;
+            int newWolf = info[node] == 1 ? 1 : 0;
+            
+            if (s + newSheep <= w + newWolf) {
                 continue;
-            }
-
-            able.remove(target);
-            able.addAll(graph.get(target));
-
-            result = Math.max(result, newSheep);
-            dfs(graph, info, new ArrayList<>(able), newSheep, newWolf);
-
-            able.removeAll(graph.get(target));
-            able.add(target);
+            } 
+            
+            List<Integer> copy = new ArrayList<>(nodes);
+            copy.addAll(graph.get(node));
+            copy.remove(Integer.valueOf(node));
+            
+            max = Math.max(s + newSheep, max);
+            
+            dfs(s + newSheep, w + newWolf, copy, info);
         }
-    }
+    } 
 }
