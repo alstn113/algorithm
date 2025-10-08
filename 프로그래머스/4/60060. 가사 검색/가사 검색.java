67ed14af75dde861579bd80dabbf9671be28402b
Minsu Kim <alstn113@gmail.com>
2025-10-08 10:27:15 +0900

    [level 4] Title: 가사 검색, Time: 675.03 ms, Memory: 512 MB -BaekjoonHub

--- 프로그래머스/4/60060. 가사 검색/가사 검색.java
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.java
@@ -1,69 +1,82 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] words, String[] queries) {
         Trie trie = new Trie();
         Trie rTrie = new Trie();
-
+        
         for (String word : words) {
             trie.insert(word);
             rTrie.insert(new StringBuilder(word).reverse().toString());
         }
-
+        
         int[] result = new int[queries.length];
         for (int i=0; i<queries.length; i++) {
             String query = queries[i];
             if (query.startsWith("?")) {
-                result[i] = rTrie.search(new StringBuilder(query).reverse().toString()) ;
+                String nq = new StringBuilder(query).reverse().toString();
+                result[i] = rTrie.search(nq);
             } else {
                 result[i] = trie.search(query);
             }
         }
-
+        
         return result;
     }
-
-    class TrieNode {
-        Map<Character, TrieNode> children = new HashMap<>();
-        int cnt = 0;
+    
+    class Node {
+        Map<Character, Node> nodes = new HashMap<>();
+        int pass = 1;
     }
-
+    
     class Trie {
-        Map<Integer, TrieNode> root = new HashMap<>();
-
+        Map<Integer, Node> map = new HashMap<>();
+        
         public void insert(String word) {
             int len = word.length();
-            TrieNode node = root.computeIfAbsent(len, v -> new TrieNode());
-            node.cnt += 1;
+            Node node;
+            if (!map.containsKey(len)) {
+                node = new Node();
+                map.put(len, node);
+            } else {
+                node = map.get(len);
+                node.pass += 1;
+            }
 
             for (Character c : word.toCharArray()) {
-                TrieNode next = node.children.computeIfAbsent(c, v -> new TrieNode());
-                next.cnt += 1;
-                node = next;
+                if (!node.nodes.containsKey(c)) {
+                    Node newNode = new Node();
+                    node.nodes.put(c, newNode);
+                    node = newNode;
+                } else {
+                    Node cur = node.nodes.get(c);
+                    cur.pass += 1;
+                    node = cur;
+                }
             }
         }
-
+        
         public int search(String query) {
             int len = query.length();
-            TrieNode node = root.get(len);
-            if (node == null) {
+            Node cur = map.get(len);
+            if (cur == null) {
                 return 0;
             }
-
+            
             for (Character c : query.toCharArray()) {
                 if (c == '?') {
-                    return node.cnt;
+                    return cur.pass;
                 }
-
-                if (node.children.get(c) == null) {
+                
+                Node next = cur.nodes.get(c);
+                if (next == null) {
                     return 0;
                 }
-                node = node.children.get(c);
+                
+                cur = next;
             }
-
-            return node.cnt;
+            
+            return cur.pass;
         }
     }
 }
