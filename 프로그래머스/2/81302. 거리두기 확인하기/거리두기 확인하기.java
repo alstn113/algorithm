79b26aa94bffa01deddc4dbf97419f88acc48546
Minsu Kim <alstn113@gmail.com>
2025-10-09 21:14:26 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.15 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,60 +1,69 @@
 import java.util.*;
 
 class Solution {
-    
     int[] dx = {1, 0, 0};
     int[] dy = {0, -1, 1};
     
     public int[] solution(String[][] places) {
-        int[] answer = new int[5];
+        int[] result = new int[5];
         
-        // 대기실 순회
-        for (int k=0; k<5; k++) {
+        for (int i=0; i<5; i++) {
+            String[] place = places[i];
             
-            // 각 대기실에 대해서
-            String[] place = places[k];
-            if (check(place)) {
-                answer[k] = 1;
+            if (!check(place)) {
+                result[i] = 0;
+            } else {
+                result[i] = 1;
             }
         }
         
-        return answer;
+        return result;
     }
-    
-    // false면 지키지 않는 것.
+
+    // check false는 거리두기 미이행
     public boolean check(String[] place) {
         for (int i=0; i<5; i++) {
-            for (int j=0; j<5; j++) {
-                if (place[i].charAt(j) == 'P') {
-                    if (bfs(i, j, place)) {
-                        return false;
-                    }
+            for (int j=0; j<5; j++) { 
+                if (place[i].charAt(j) != 'P') {
+                    continue;
                 }
+                
+                if (!bfs(place, i, j)) {
+                    return false;
+                }                    
             }
         }
         
         return true;
     }
     
-    // true 면 지키지 않는 것.
-    public boolean bfs(int x, int y, String[] place) {
-        boolean[][] visited = new boolean[5][5];
+    // 거리두기 미이행 시 false;
+    public boolean bfs(String[] place, int x, int y) {
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[] {x, y, 0});
-        visited[x][y] = true;
         
         while(!q.isEmpty()) {
             int[] cur = q.poll();
+            
+            if (cur[2] >= 1) {
+                if (place[cur[0]].charAt(cur[1]) == 'P') {
+                    return false;
+                }
+                
+                if (cur[2] == 2) {
+                    continue;
+                }
+            }
             
             for (int i=0; i<3; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
                 
-                if (nx < 0 || nx >=5 || ny < 0 || ny >= 5) {
+                if (nx == x && ny == y) {
                     continue;
                 }
                 
-                if (visited[nx][ny]) {
+                if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) {
                     continue;
                 }
                 
@@ -62,21 +71,13 @@ class Solution {
                     continue;
                 }
                 
-                if (place[nx].charAt(ny) == 'P') {
-                    return true; // 거리두기 지키지 않음.
-                }
-
-                visited[nx][ny] = true;
-                if (cur[2] < 1) {
-                    q.offer(new int[] { nx, ny, cur[2] + 1});
-                }
+                q.offer(new int[] {nx, ny, cur[2] + 1});
             }
         }
         
-        return false;
+        return true;
     }
 }
-
 
 
 
