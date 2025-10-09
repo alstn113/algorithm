da7908427f17483f2fc2ab0ad20f0c142aa6e94a
Minsu Kim <alstn113@gmail.com>
2025-10-09 20:50:47 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.83 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,44 +1,44 @@
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int n = board.length;
-        int[] targets = new int[board.length];
-        Arrays.fill(targets, -1);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        int[] arr = new int[n];
+        Arrays.fill(arr, -1);
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
                 if (board[j][i] != 0) {
-                    targets[i] = j;
+                    arr[i] = j;
                     break;
                 }
             }
         }
-
-        int answer = 0;
+        
+        
         Stack<Integer> st = new Stack<>();
-        for (int m : moves) {
-            int v = targets[m - 1];
-            if (targets[m - 1] == -1) {
+        int result = 0;
+        
+        for (int move : moves) {
+            if (arr[move-1] == -1) {
                 continue;
             }
-
-            int c = board[v][m - 1];
-            if (!st.isEmpty() && c == st.peek()) {
-                st.pop();
-                answer += 2;
+            
+            int v = board[arr[move-1]][move-1];
+            if (arr[move-1] == n-1) {
+                arr[move-1] = -1;
             } else {
-                st.push(c);
+                arr[move-1] += 1;
             }
-
-            if (targets[m - 1] == n - 1) {
-                targets[m - 1] = -1;
+            
+            if (!st.isEmpty() && v == st.peek()) {
+                st.pop();
+                result += 2;        
             } else {
-                targets[m - 1] += 1;
+                st.push(v);
             }
         }
-
-        return answer;
+        
+        return result;
     }
 }
