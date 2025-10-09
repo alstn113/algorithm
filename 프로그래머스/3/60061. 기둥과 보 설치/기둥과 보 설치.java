bfb5b16d569d62e1c5184c09dd87d33cc479194d
Minsu Kim <alstn113@gmail.com>
2025-10-09 10:18:40 +0900

    [level 3] Title: 기둥과 보 설치, Time: 49.61 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
@@ -1,75 +1,77 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-
-    boolean[][][] frames;
-
     public int[][] solution(int n, int[][] build_frame) {
-        frames = new boolean[n + 1][n + 1][2];
-
-        for (int[] frame : build_frame) {
-            int x = frame[0];
-            int y = frame[1];
-            int type = frame[2];
-            int cd = frame[3];
-
-            if (cd == 1) {
-                if (type == 0) {
-                    if (canBuildGi(x, y)) {
-                        frames[y][x][0] = true;
-                    }
-                } else {
-                    if (canBuildBo(x, y, n)) {
-                        frames[y][x][1] = true;
-                    }
+        boolean[][][] board = new boolean[n+1][n+1][2];
+        
+        // 로직 실행 시 세로에 -n을 하고 출력 시 +n 을 해야함.
+        for (int[] bf : build_frame) {
+            int x = bf[0]; // 가로
+            int y = n - bf[1]; // 세로
+            int a = bf[2]; // 0은 기둥, 1은 보 
+            int b = bf[3]; // 0은 삭제, 1은 설치
+            
+            if (b == 1) {
+                if (check(board, y, x, a, n)) {
+                    board[y][x][a] = true;
                 }
-            }
-            else {
-                frames[y][x][type] = false;
-                if (!canBuild(n)) {
-                    frames[y][x][type] = true;
+            } else {
+                // 삭제 가능한가?
+                board[y][x][a] = false;
+                if (!allcheck(board, n)) {
+                    board[y][x][a] = true;
                 }
             }
         }
-
+        
         List<int[]> result = new ArrayList<>();
-        for (int x = 0; x <= n; x++) {
-            for (int y = 0; y <= n; y++) {
-                for (int t = 0; t < 2; t++) {
-                    if (frames[y][x][t]) {
-                        result.add(new int[]{x, y, t});
+        for (int j=0; j<n+1; j++) {
+            for (int i=n; i>-1; i--) {
+                for (int k=0; k<2; k++) {
+                    if (board[i][j][k]) {
+                        result.add(new int[] {j, n - i, k});
                     }
                 }
             }
         }
 
-        return result.toArray(new int[result.size()][]);
+        return result.stream().toArray(int[][]::new);
     }
-
-    public boolean canBuild(int n) {
-        for (int y = 0; y <= n; y++) {
-            for (int x = 0; x <= n; x++) {
-                if (frames[y][x][0] && !canBuildGi(x, y)) {
-                    return false;
-                }
-                if (frames[y][x][1] && !canBuildBo(x, y, n)) {
-                    return false;
+    
+    public boolean check(boolean[][][] board, int y, int x, int a, int n) {
+        if (a == 0) { // 기둥 - 바닥에 있거나 보 위에 있거나 기둥 위에 있거나 
+            if (y == n || 
+                (x < n && board[y][x][1]) || 
+                (x > 0 && board[y][x-1][1]) || 
+                (y != n && board[y+1][x][0])
+               ) 
+            {
+                return true;
+            }
+        } else { // 보
+            // 왼쪽 또는 오른쪽에 기둥이 있는가? 
+            // 보 중간인 경우
+            if (
+                board[y+1][x][0] || 
+                board[y+1][x+1][0] || 
+                (0<x && x<n && board[y][x-1][1] && board[y][x+1][1])
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean allcheck(boolean[][][] board, int n) {
+        for (int i=0; i<n+1; i++) {
+            for (int j=0; j<n+1; j++) {
+                for (int k=0; k<2; k++) {
+                    if (board[i][j][k] && !check(board, i, j, k, n)) { 
+                        return false;
+                    }
                 }
             }
         }
         return true;
-    }
-
-    public boolean canBuildGi(int x, int y) {
-        return y == 0
-                || (y > 0 && frames[y - 1][x][0])
-                || (x > 0 && frames[y][x - 1][1])
-                || frames[y][x][1];
-    }
-
-    public boolean canBuildBo(int x, int y, int n) {
-        return (y > 0 && (frames[y - 1][x][0] || frames[y - 1][x + 1][0])) // 한쪽 끝이 기둥 위
-                || (x > 0 && x < n && frames[y][x - 1][1] && frames[y][x + 1][1]); // 양쪽 보 연결
     }
 }
