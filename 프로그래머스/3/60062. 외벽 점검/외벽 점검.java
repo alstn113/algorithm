c5a214d200a6c5cbd267fd36ad90c64f0d06d2dc
Minsu Kim <alstn113@gmail.com>
2025-10-23 11:02:12 +0900

    [level 3] Title: 외벽 점검, Time: 79.87 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Solution {
+
+    int INF = (int) 1e9;
+    List<List<Integer>> perm = new ArrayList<>();
+    
+    public int solution(int n, int[] weak, int[] dist) {
+        boolean[] visited = new boolean[dist.length];
+        dfs(0, new ArrayList<>(), 0, dist, visited);
+        
+        int[] weak2 = new int[weak.length * 2];
+        for (int i=0; i<weak.length; i++) {
+            weak2[i] = weak[i];
+            weak2[i+weak.length] = weak[i] + n;
+        }
+        
+        int wLen = weak2.length;
+        int answer = INF;
+        
+        for (List<Integer> p : perm) {
+            
+            for (int i=0; i<weak.length; i++) {
+                int dpoint = 0; 
+                int w=0;
+                while (w < weak.length && dpoint < p.size()) {
+                    int d = weak2[i+w] + p.get(dpoint);
+                    w += 1;
+                    
+                    while (w < weak.length && weak2[i+w] <= d) {
+                        w += 1;
+                    }
+                    
+                    dpoint += 1;
+                }
+                
+                if (w == weak.length) {
+                    answer = Math.min(answer, dpoint);
+                }
+            }            
+            
+        }
+        
+        if (answer == INF) {
+            return -1;
+        }
+        
+        return answer;
+    }
+    
+    public void dfs(int cnt, List<Integer> cur, int start, int[] dist, boolean[] visited) {
+        if (cnt == dist.length) {
+            perm.add(new ArrayList<>(cur));
+            return;
+        }
+        
+        for (int i=0; i<dist.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            
+            cur.add(dist[i]);
+            visited[i] = true;
+            dfs(cnt+1, cur, i+1, dist, visited);
+            visited[i] = false;
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
