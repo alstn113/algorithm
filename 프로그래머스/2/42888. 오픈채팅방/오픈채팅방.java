6464c74b9afa9e3bacbda95c33b5045768ec29e1
Minsu Kim <alstn113@gmail.com>
2025-10-23 16:02:19 +0900

    [level 2] Title: 오픈채팅방, Time: 438.58 ms, Memory: 266 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,41 +2,34 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-        Map<String, String> ids = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
+        
         List<List<String>> result = new ArrayList<>();
         
         for (String r : record) {
-            String[] split = r.split(" ");
-            
-            String action = split[0];
-            String id = split[1];
-            
-            if (action.equals("Enter")) {
-                String name = split[2];
-                ids.put(id, name);                
-                result.add(List.of("Enter", id));
-                continue;
-            }
-            
-            if (action.equals("Change")) {
-                String name = split[2];
-                ids.put(id, name);
-                continue;
-            }
-            
-            if (action.equals("Leave")) {
-                result.add(List.of("Leave", id));
-            }
-        }
-
-        String[] answer = new String[result.size()];
-        for (int i=0; i<result.size(); i++) {
-            if (result.get(i).get(0).equals("Enter")) {
-                answer[i] = String.format("%s님이 들어왔습니다.", ids.get(result.get(i).get(1)));
+            if (r.startsWith("Enter")) {
+                String[] split = r.split(" ");
+                map.put(split[1], split[2]);
+                result.add(List.of("Enter", split[1]));
+            } else if (r.startsWith("Change")) {
+                String[] split = r.split(" ");
+                map.put(split[1], split[2]);
             } else {
-                answer[i] = String.format("%s님이 나갔습니다.", ids.get(result.get(i).get(1)));
+                String[] split = r.split(" ");               
+                result.add(List.of("Leave", split[1]));
             }
         }
-        return answer;
+        
+        String[] arr = new String[result.size()];
+        for (int i=0; i<result.size(); i++) {
+            String id = map.get(result.get(i).get(1));
+            if (result.get(i).get(0).equals("Enter")) {
+                arr[i] = String.format("%s님이 들어왔습니다.", id);
+            } else {
+                arr[i] = String.format("%s님이 나갔습니다.", id);                               
+            }
+        }
+        
+        return arr;
     }
 }
